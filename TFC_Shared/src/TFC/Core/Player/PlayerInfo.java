44ca2b98e32b1c60d1e90438eacfdd8a4c23184d
Robert Anthony <Bioxx2007@gmail.com>
2013-04-04 08:09:23 -0400

    Knapping interface converted to use buttons

--- TFC_Shared/src/TFC/Core/Player/PlayerInfo.java
+++ TFC_Shared/src/TFC/Core/Player/PlayerInfo.java
@@ -1,38 +1,8 @@
 package TFC.Core.Player;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.INetworkManager;
 import TFC.Core.TFC_Time;
-import TFC.Items.ItemChisel;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.entity.*;
-import net.minecraft.client.gui.inventory.*;
-import net.minecraft.block.*;
-import net.minecraft.block.material.*;
-import net.minecraft.crash.*;
-import net.minecraft.creativetab.*;
-import net.minecraft.entity.*;
-import net.minecraft.entity.ai.*;
-import net.minecraft.entity.effect.*;
-import net.minecraft.entity.item.*;
-import net.minecraft.entity.monster.*;
-import net.minecraft.entity.player.*;
-import net.minecraft.entity.projectile.*;
-import net.minecraft.inventory.*;
-import net.minecraft.item.*;
-import net.minecraft.nbt.*;
-import net.minecraft.network.*;
-import net.minecraft.network.packet.*;
-import net.minecraft.pathfinding.*;
-import net.minecraft.potion.*;
-import net.minecraft.server.*;
-import net.minecraft.stats.*;
-import net.minecraft.tileentity.*;
-import net.minecraft.util.*;
-import net.minecraft.village.*;
-import net.minecraft.world.*;
-import net.minecraft.world.biome.*;
-import net.minecraft.world.chunk.*;
-import net.minecraft.world.gen.feature.*;
 
 public class PlayerInfo
 {
@@ -45,6 +15,7 @@ public class PlayerInfo
 	public int lockZ = -9999999;
 
 	public ItemStack knappingRockType;
+	
 	public INetworkManager networkManager;
 	private long lastChange;
 
