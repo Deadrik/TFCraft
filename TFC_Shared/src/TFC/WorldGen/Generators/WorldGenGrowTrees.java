714fae3a3a44efaf501321f86bd9f3c8d751ca4a
Robert Anthony <Bioxx2007@gmail.com>
2013-04-05 06:06:55 -0400

    Tree growth no longer uses Terrain Adjusted Rainfall

--- TFC_Shared/src/TFC/WorldGen/Generators/WorldGenGrowTrees.java
+++ TFC_Shared/src/TFC/WorldGen/Generators/WorldGenGrowTrees.java
@@ -2,43 +2,12 @@ package TFC.WorldGen.Generators;
 
 import java.util.Random;
 
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
 import TFC.TFCBlocks;
 import TFC.Core.TFC_Climate;
 import TFC.Core.TFC_Core;
 import TFC.Enums.EnumTree;
-import TFC.WorldGen.DataLayer;
-import TFC.WorldGen.TFCBiome;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.entity.*;
-import net.minecraft.client.gui.inventory.*;
-import net.minecraft.block.*;
-import net.minecraft.block.material.*;
-import net.minecraft.crash.*;
-import net.minecraft.creativetab.*;
-import net.minecraft.entity.*;
-import net.minecraft.entity.ai.*;
-import net.minecraft.entity.effect.*;
-import net.minecraft.entity.item.*;
-import net.minecraft.entity.monster.*;
-import net.minecraft.entity.player.*;
-import net.minecraft.entity.projectile.*;
-import net.minecraft.inventory.*;
-import net.minecraft.item.*;
-import net.minecraft.nbt.*;
-import net.minecraft.network.*;
-import net.minecraft.network.packet.*;
-import net.minecraft.pathfinding.*;
-import net.minecraft.potion.*;
-import net.minecraft.server.*;
-import net.minecraft.stats.*;
-import net.minecraft.tileentity.*;
-import net.minecraft.util.*;
-import net.minecraft.village.*;
-import net.minecraft.world.*;
-import net.minecraft.world.biome.*;
-import net.minecraft.world.chunk.*;
-import net.minecraft.world.gen.feature.*;
 
 public class WorldGenGrowTrees 
 {
@@ -67,7 +36,7 @@ public class WorldGenGrowTrees
 		
 		if(hasSpaceToGrow)
 		{
-			float rainfall = TFC_Climate.getTerrainAdjustedRainfall(xCoord, yCoord, zCoord);
+			float rainfall = TFC_Climate.getRainfall(xCoord, yCoord, zCoord);
 			float temperature = TFC_Climate.getBioTemperatureHeight(xCoord, yCoord, zCoord);
 			float evt = TFC_Climate.manager.getEVTLayerAt(xCoord, zCoord).floatdata1;
 			
