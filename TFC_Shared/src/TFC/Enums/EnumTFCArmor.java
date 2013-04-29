13ad4c094d0aa38e8b185ca02edd4a56d86dee43
Robert Anthony <Bioxx2007@gmail.com>
2013-04-28 23:08:41 -0400

    Started TFC Armor Enum

--- TFC_Shared/src/TFC/Enums/EnumTFCArmor.java
+++ TFC_Shared/src/TFC/Enums/EnumTFCArmor.java
@@ -2,16 +2,15 @@ package TFC.Enums;
 
 public enum EnumTFCArmor 
 {
-	CopperPlate			(200, 200, 200, EnumMetalType.COPPER),
-	BismuthBronzePlate	(200, 200, 200, EnumMetalType.BISMUTHBRONZE),
-	BlackBronzePlate	(200, 200, 200, EnumMetalType.BLACKBRONZE),
-	BronzePlate			(200, 200, 200, EnumMetalType.BRONZE),
-	WroughtIronPlate	(200, 200, 200, EnumMetalType.WROUGHTIRON),
-	SteelPlate			(200, 200, 200, EnumMetalType.STEEL),
-	RoseGoldPlate		(200, 200, 200, EnumMetalType.ROSEGOLD),
-	BlackSteelPlate		(200, 200, 200, EnumMetalType.BLACKSTEEL),
-	BlueSteelPlate		(200, 200, 200, EnumMetalType.BLUESTEEL),
-	RedSteelPlate		(200, 200, 200, EnumMetalType.REDSTEEL);
+	CopperPlate			(200, 200, 200, EnumMetalType.COPPER),//Tier 0
+	BismuthBronzePlate	(200, 200, 200, EnumMetalType.BISMUTHBRONZE),//Tier 1
+	BlackBronzePlate	(200, 200, 200, EnumMetalType.BLACKBRONZE),//Tier 1
+	BronzePlate			(200, 200, 200, EnumMetalType.BRONZE),//Tier 1
+	WroughtIronPlate	(200, 200, 200, EnumMetalType.WROUGHTIRON),//Tier 2
+	SteelPlate			(200, 200, 200, EnumMetalType.STEEL),//Tier 3
+	BlackSteelPlate		(200, 200, 200, EnumMetalType.BLACKSTEEL),//Tier 4
+	BlueSteelPlate		(200, 200, 200, EnumMetalType.BLUESTEEL),//Tier 5
+	RedSteelPlate		(200, 200, 200, EnumMetalType.REDSTEEL);//Tier 5
 	
 	private final int armorRatingPiercing;
 	private final int armorRatingSlashing;
