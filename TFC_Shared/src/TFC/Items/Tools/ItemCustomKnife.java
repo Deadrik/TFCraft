3067fee43c56cd273cdf606585bf17d18c6ddf54
Robert Anthony <Bioxx2007@gmail.com>
2013-05-13 08:05:53 -0400

    Fixed knife replacing partial blocks such as tool racks when trying to make food prep area.

--- TFC_Shared/src/TFC/Items/Tools/ItemCustomKnife.java
+++ TFC_Shared/src/TFC/Items/Tools/ItemCustomKnife.java
@@ -1,44 +1,12 @@
 package TFC.Items.Tools;
 
-import java.util.List;
-
-import TFC.*;
-import TFC.Blocks.BlockSlab;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import TFC.TFCBlocks;
 import TFC.Core.TFC_Core;
-import TFC.Core.TFC_Settings;
 import TFC.Enums.EnumSize;
-import TFC.TileEntities.TileEntityPartial;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.entity.*;
-import net.minecraft.client.gui.inventory.*;
-import net.minecraft.block.*;
-import net.minecraft.block.material.*;
-import net.minecraft.crash.*;
-import net.minecraft.creativetab.*;
-import net.minecraft.entity.*;
-import net.minecraft.entity.ai.*;
-import net.minecraft.entity.effect.*;
-import net.minecraft.entity.item.*;
-import net.minecraft.entity.monster.*;
-import net.minecraft.entity.player.*;
-import net.minecraft.entity.projectile.*;
-import net.minecraft.inventory.*;
-import net.minecraft.item.*;
-import net.minecraft.nbt.*;
-import net.minecraft.network.*;
-import net.minecraft.network.packet.*;
-import net.minecraft.pathfinding.*;
-import net.minecraft.potion.*;
-import net.minecraft.server.*;
-import net.minecraft.stats.*;
-import net.minecraft.tileentity.*;
-import net.minecraft.util.*;
-import net.minecraft.village.*;
-import net.minecraft.world.*;
-import net.minecraft.world.biome.*;
-import net.minecraft.world.chunk.*;
-import net.minecraft.world.gen.feature.*;
 
 public class ItemCustomKnife extends ItemWeapon
 {
@@ -54,6 +22,7 @@ public class ItemCustomKnife extends ItemWeapon
 		return "/bioxx/terratools.png";
 	}
 
+	@Override
 	public EnumSize getSize()
 	{
 		return EnumSize.SMALL;
@@ -71,7 +40,7 @@ public class ItemCustomKnife extends ItemWeapon
 		int id = world.getBlockId(x, y, z);
 		if(!world.isRemote && id != TFCBlocks.ToolRack.blockID)
 		{
-			if(side == 1 && !TFC_Core.isSoil(id) && !TFC_Core.isWater(id))
+			if(side == 1 && !TFC_Core.isSoil(id) && !TFC_Core.isWater(id) && world.getBlockId(x, y+1, z) == 0)
 			{
 				world.setBlock(x, y+1, z, TFCBlocks.FoodPrep.blockID);
 				world.markBlockForUpdate(x, y+1, z);
