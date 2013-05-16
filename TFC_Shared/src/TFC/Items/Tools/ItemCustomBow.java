ed133c2307e25395a79f2e2cb5f84a1306d5e7e7
Robert Anthony <Bioxx2007@gmail.com>
2013-05-15 20:40:36 -0400

    Fixed bows doing vanilla damage

--- TFC_Shared/src/TFC/Items/Tools/ItemCustomBow.java
+++ TFC_Shared/src/TFC/Items/Tools/ItemCustomBow.java
@@ -1,10 +1,18 @@
 package TFC.Items.Tools;
 
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ArrowLooseEvent;
 import TFC.Core.TFCTabs;
+import TFC.Entities.EntityArrowTFC;
 import TFC.Enums.EnumSize;
 import TFC.Enums.EnumWeight;
 import TFC.Items.ISize;
@@ -24,6 +32,81 @@ public class ItemCustomBow extends ItemBow implements ISize
         this.maxStackSize = 1;
         this.setMaxDamage(384);
         setCreativeTab(TFCTabs.TFCTools);
+    }
+    
+    @Override
+	public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer, int par4)
+    {
+        int j = this.getMaxItemUseDuration(par1ItemStack) - par4;
+
+        ArrowLooseEvent event = new ArrowLooseEvent(par3EntityPlayer, par1ItemStack, j);
+        MinecraftForge.EVENT_BUS.post(event);
+        if (event.isCanceled())
+        {
+            return;
+        }
+        j = event.charge;
+
+        boolean flag = par3EntityPlayer.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, par1ItemStack) > 0;
+
+        if (flag || par3EntityPlayer.inventory.hasItem(Item.arrow.itemID))
+        {
+            float f = j / 20.0F;
+            f = (f * f + f * 2.0F) / 3.0F;
+
+            if (f < 0.1D)
+            {
+                return;
+            }
+
+            if (f > 1.0F)
+            {
+                f = 1.0F;
+            }
+
+            EntityArrowTFC entityarrow = new EntityArrowTFC(par2World, par3EntityPlayer, f * 2.0F);
+
+            if (f == 1.0F)
+            {
+                entityarrow.setIsCritical(true);
+            }
+
+            int k = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, par1ItemStack);
+
+            if (k > 0)
+            {
+                entityarrow.setDamage(entityarrow.getDamage() + k * 0.5D + 0.5D);
+            }
+
+            int l = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, par1ItemStack);
+
+            if (l > 0)
+            {
+                entityarrow.setKnockbackStrength(l);
+            }
+
+            if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, par1ItemStack) > 0)
+            {
+                entityarrow.setFire(100);
+            }
+
+            par1ItemStack.damageItem(1, par3EntityPlayer);
+            par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+
+            if (flag)
+            {
+                entityarrow.canBePickedUp = 2;
+            }
+            else
+            {
+                par3EntityPlayer.inventory.consumeInventoryItem(Item.arrow.itemID);
+            }
+
+            if (!par2World.isRemote)
+            {
+                par2World.spawnEntityInWorld(entityarrow);
+            }
+        }
     }
 
     @Override
