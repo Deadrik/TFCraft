bc9ee685020e3135056adb1ec222d002fb383714
Tobias Wildgruber <tow@conject.com>
2013-06-08 19:09:12 +0200

    - fix for: sound dies sporadicly, needs restart to fix (sound system's worker thread died when trying to play a TerraFirmaCraft.ogg -> null was passed as URL - wrong capitalization in getResource())

--- TFC_Shared/src/TFC/Core/TFC_Sounds.java
+++ TFC_Shared/src/TFC/Core/TFC_Sounds.java
@@ -1,12 +1,12 @@
 package TFC.Core;
 
-public class TFC_Sounds 
+public class TFC_Sounds
 {
 	private static final String LOCATION = "mods/TerraFirmaCraft/sounds/tfc/";
 	private static final String PREFIX = "mods.TerraFirmaCraft.sounds.tfc.";
 	private static final String LOCATION_MUSIC = "mods/TerraFirmaCraft/sounds/tfc/music/";
 	private static final String PREFIX_MUSIC = "mods.TerraFirmaCraft.sounds.tfc.music.";
-	
+
 	public static String [] soundFiles = {
 			LOCATION + "fallingrockshort1.ogg",
 			LOCATION + "fallingrockshort2.ogg",
@@ -20,15 +20,15 @@ public class TFC_Sounds
 			LOCATION + "metalimpact4.ogg",
 			LOCATION + "stonedrag1.ogg",
 			LOCATION + "stonedrag2.ogg"};
-	
+
 	public static String [] musicFiles = {
 		LOCATION_MUSIC + "Through the Willows.ogg",
 		LOCATION_MUSIC + "FirmaVista.ogg",
 		LOCATION_MUSIC + "Sycamore Heights.ogg",
 		LOCATION_MUSIC + "Dreams of the Phae.ogg",
-		LOCATION_MUSIC + "Terrafirmacraft.ogg"
+		LOCATION_MUSIC + "TerraFirmaCraft.ogg"
 	};
-	
+
 	public static final String FALLININGROCKSHORT = PREFIX + "fallingrockshort";
 	public static final String FALLININGROCKLONG = PREFIX + "fallingrocklong";
 	public static final String FALLININGDIRTSHORT = PREFIX + "fallingdirtshort";
