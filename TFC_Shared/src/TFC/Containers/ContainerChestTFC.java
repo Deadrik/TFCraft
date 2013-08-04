f38fbb9ddcc7c44b0437c4d6cf1a222694f790fc
Robert Anthony <Bioxx2007@gmail.com>
2013-08-03 22:52:09 -0400

    Chests can now fit objects up to size LARGE except for tools/weapons which they now only support up to SMALL. Hammers are now SMALL

--- TFC_Shared/src/TFC/Containers/ContainerChestTFC.java
+++ TFC_Shared/src/TFC/Containers/ContainerChestTFC.java
@@ -1,41 +1,17 @@
 package TFC.Containers;
 
 import java.util.ArrayList;
-import TFC.*;
-import TFC.API.Enums.EnumSize;
-import TFC.Containers.Slots.SlotSize;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryLargeChest;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import TFC.TFCItems;
+import TFC.Containers.Slots.SlotChest;
 import TFC.TileEntities.TileEntityChestTFC;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.entity.*;
-import net.minecraft.client.gui.inventory.*;
-import net.minecraft.block.*;
-import net.minecraft.block.material.*;
-import net.minecraft.crash.*;
-import net.minecraft.creativetab.*;
-import net.minecraft.entity.*;
-import net.minecraft.entity.ai.*;
-import net.minecraft.entity.effect.*;
-import net.minecraft.entity.item.*;
-import net.minecraft.entity.monster.*;
-import net.minecraft.entity.player.*;
-import net.minecraft.entity.projectile.*;
-import net.minecraft.inventory.*;
-import net.minecraft.item.*;
-import net.minecraft.nbt.*;
-import net.minecraft.network.*;
-import net.minecraft.network.packet.*;
-import net.minecraft.pathfinding.*;
-import net.minecraft.potion.*;
-import net.minecraft.server.*;
-import net.minecraft.stats.*;
-import net.minecraft.tileentity.*;
-import net.minecraft.util.*;
-import net.minecraft.village.*;
-import net.minecraft.world.*;
-import net.minecraft.world.biome.*;
-import net.minecraft.world.chunk.*;
-import net.minecraft.world.gen.feature.*;
 
 public class ContainerChestTFC extends ContainerTFC
 {
@@ -102,7 +78,7 @@ public class ContainerChestTFC extends ContainerTFC
         {
             for (var5 = 0; var5 < 9; ++var5)
             {
-                this.addSlotToContainer(new SlotSize(lowerChestInventory, var5 + var4 * 9, 8 + var5 * 18, 18 + var4 * 18).setSize(EnumSize.MEDIUM).addItemException(exceptions));
+                this.addSlotToContainer(new SlotChest(lowerChestInventory, var5 + var4 * 9, 8 + var5 * 18, 18 + var4 * 18).addItemException(exceptions));
             }
         }
 
