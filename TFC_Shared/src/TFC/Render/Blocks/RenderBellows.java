cc2553d8a4111e98f990fdda0c800129ad6d4c9c
emris <emris@pc-net.at>
2013-08-11 13:54:58 +0200

    Changed bellows animation
    + Animation is done with TileEntitySpecialRenderer now
    + Fixed inventory rendering

--- TFC_Shared/src/TFC/Render/Blocks/RenderBellows.java
+++ TFC_Shared/src/TFC/Render/Blocks/RenderBellows.java
@@ -25,21 +25,63 @@ public class RenderBellows implements ISimpleBlockRenderingHandler {
 		//forward
 		renderer.overrideBlockTexture = BlockBellows.BellowsFront;
 		renderer.setRenderBounds(0.0F, 0.0F, 0.9F, 1.0F, 1.0F, 1.0F);
-		renderInvBlock(block, metadata, renderer);
+		renderInvBlock(block, renderer);
 		//mid
 		renderer.clearOverrideBlockTexture();
 		renderer.setRenderBounds(0.1F, 0.1F, 0.05F, 0.9F, 0.9F, 0.95F);
-		renderInvBlock(block, 0, renderer);
+		renderInvBlock(block, renderer);
 		//back
 		renderer.overrideBlockTexture = BlockBellows.BellowsBack;
 		renderer.setRenderBounds(0.0F, 0.0F, 0, 1.0F, 1.0F, 0.1F);
-		renderInvBlock(block, metadata, renderer);
+		renderInvBlock(block, renderer);
 		
 		renderer.clearOverrideBlockTexture();
 	}
 
 	@Override
 	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
+		//renderBlock(modelId, renderer, block, x, y, z);
+		return false;
+	}
+
+	@Override
+	public int getRenderId() {
+		return 0;
+	}
+	
+	public static void renderInvBlock(Block block, RenderBlocks renderer)
+	{
+		Tessellator var14 = Tessellator.instance;
+		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+		var14.startDrawingQuads();
+		var14.setNormal(0.0F, -1.0F, 0.0F);
+		renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, 2));
+		var14.draw();
+		var14.startDrawingQuads();
+		var14.setNormal(0.0F, 1.0F, 0.0F);
+		renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, 2));
+		var14.draw();
+		var14.startDrawingQuads();
+		var14.setNormal(-1.0F, 0.0F, 0.0F);
+		renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, 1));
+		var14.draw();
+		var14.startDrawingQuads();
+		var14.setNormal(0.0F, 0.0F, -1.0F);
+		renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, 3));
+		var14.draw();
+		var14.startDrawingQuads();
+		var14.setNormal(-1.0F, 0.0F, 0.0F);
+		renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, 0));
+		var14.draw();
+		var14.startDrawingQuads();
+		var14.setNormal(0.0F, 0.0F, 1.0F);
+		renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, 0));
+		var14.draw();
+		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+	}
+	
+	private void renderBlock(int modelId, RenderBlocks renderer, Block block, int x, int y, int z)
+	{
 		if(modelId == TFCBlocks.BellowsRenderId) {
 			IBlockAccess blockAccess = renderer.blockAccess;
 			int meta = blockAccess.getBlockMetadata(x, y, z);
@@ -120,42 +162,5 @@ public class RenderBellows implements ISimpleBlockRenderingHandler {
 				renderer.setRenderBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
 			}
 		}
-		return true;
-	}
-
-	@Override
-	public int getRenderId() {
-		return 0;
-	}
-	
-	public static void renderInvBlock(Block block, int m, RenderBlocks renderer)
-	{
-		Tessellator var14 = Tessellator.instance;
-		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
-		var14.startDrawingQuads();
-		var14.setNormal(0.0F, -1.0F, 0.0F);
-		renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, m));
-		var14.draw();
-		var14.startDrawingQuads();
-		var14.setNormal(0.0F, 1.0F, 0.0F);
-		renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(1, m));
-		var14.draw();
-		var14.startDrawingQuads();
-		var14.setNormal(-1.0F, 0.0F, 0.0F);
-		renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(2, m));
-		var14.draw();
-		var14.startDrawingQuads();
-		var14.setNormal(0.0F, 0.0F, -1.0F);
-		renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(3, m));
-		var14.draw();
-		var14.startDrawingQuads();
-		var14.setNormal(-1.0F, 0.0F, 0.0F);
-		renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(4, m));
-		var14.draw();
-		var14.startDrawingQuads();
-		var14.setNormal(0.0F, 0.0F, 1.0F);
-		renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(5, m));
-		var14.draw();
-		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
 	}
 }
