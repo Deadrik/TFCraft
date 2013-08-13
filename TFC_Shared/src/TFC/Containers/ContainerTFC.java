a856a8a0fa0ab514b09c494abf1b5efd68fd3fa2
Robert Anthony <Bioxx2007@gmail.com>
2013-08-13 10:02:13 -0400

    Readded a removed method to ContainerTFC

--- TFC_Shared/src/TFC/Containers/ContainerTFC.java
+++ TFC_Shared/src/TFC/Containers/ContainerTFC.java
@@ -16,6 +16,119 @@ public class ContainerTFC extends Container
 	}
 
 	@Override
+	protected boolean mergeItemStack(ItemStack is, int slotStart, int slotFinish, boolean par4)
+	{
+		boolean var5 = false;
+		int slotIndex = slotStart;
+
+		if (par4)
+		{
+			slotIndex = slotFinish - 1;
+		}
+
+		Slot slot;
+		ItemStack slotstack;
+
+		if (is.isStackable())
+		{
+			while (is.stackSize > 0 && (!par4 && slotIndex < slotFinish || par4 && slotIndex >= slotStart))
+			{
+				slot = (Slot)this.inventorySlots.get(slotIndex);
+				slotstack = slot.getStack();
+
+				if (slotstack != null
+						&& slotstack.itemID == is.itemID
+						//                	&& !is.getHasSubtypes()
+						&& is.getItemDamage() == slotstack.getItemDamage()
+						&& ItemStack.areItemStackTagsEqual(is, slotstack)
+						&& slotstack.stackSize < slot.getSlotStackLimit())
+				{
+					int mergedStackSize = is.stackSize + getSmaller(slotstack.stackSize, slot.getSlotStackLimit());
+
+					//First we check if we can add the two stacks together and the resulting stack is smaller than the maximum size for the slot or the stack
+					if (mergedStackSize <= is.getMaxStackSize() && mergedStackSize <= slot.getSlotStackLimit())
+					{
+						is.stackSize = 0;
+						slotstack.stackSize = mergedStackSize;
+						slot.onSlotChanged();
+						var5 = true;
+					}
+					else if (slotstack.stackSize < is.getMaxStackSize())
+					{
+						is.stackSize -= is.getMaxStackSize() - slotstack.stackSize;
+						slotstack.stackSize = is.getMaxStackSize();
+						slot.onSlotChanged();
+						var5 = true;
+					}
+				}
+
+				if (par4)
+				{
+					--slotIndex;
+				}
+				else
+				{
+					++slotIndex;
+				}
+			}
+		}
+
+		if (is.stackSize > 0)
+		{
+			if (par4)
+			{
+				slotIndex = slotFinish - 1;
+			}
+			else
+			{
+				slotIndex = slotStart;
+			}
+
+			while (!par4 && slotIndex < slotFinish || par4 && slotIndex >= slotStart)
+			{
+				slot = (Slot)this.inventorySlots.get(slotIndex);
+				slotstack = slot.getStack();
+				if (slotstack == null && slot.isItemValid(is) && slot.getSlotStackLimit() < is.stackSize)
+				{
+					slot.putStack(is.copy());
+					slot.onSlotChanged();
+					is.stackSize -= slot.getSlotStackLimit();
+					var5 = true;
+					break;
+				}
+				else if (slotstack == null && slot.isItemValid(is))
+				{
+					slot.putStack(is.copy());
+					slot.onSlotChanged();
+					is.stackSize = 0;
+					var5 = true;
+					break;
+				}
+
+				if (par4)
+				{
+					--slotIndex;
+				}
+				else
+				{
+					++slotIndex;
+				}
+			}
+		}
+
+		return var5;
+	}
+
+	protected int getSmaller(int i, int j)
+	{
+		if(i < j) {
+			return i;
+		} else {
+			return j;
+		}
+	}
+
+	@Override
 	public void detectAndSendChanges()
 	{
 		for (int i = 0; i < this.inventorySlots.size(); ++i)
