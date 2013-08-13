008d306fac48e4261368b8c2f7b6e4511f758a8a
Robert Anthony <Bioxx2007@gmail.com>
2013-08-13 07:40:14 -0400

    Resolves #184

--- TFC_Shared/src/TFC/Items/ItemOreSmall.java
+++ TFC_Shared/src/TFC/Items/ItemOreSmall.java
@@ -12,34 +12,59 @@ import TFC.API.Enums.EnumWeight;
 
 public class ItemOreSmall extends ItemOre
 {
-    public ItemOreSmall(int i)
-    {
-        super(i);
-    }
+	public ItemOreSmall(int i)
+	{
+		super(i);
+	}
 
-    @Override
-    public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List list)
-    {
-        for(int i = 0; i < 14; i++) {
-            list.add(new ItemStack(this,1,i));}
-    }
+	@Override
+	public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List list)
+	{
+		for(int i = 0; i < 14; i++) {
+			list.add(new ItemStack(this,1,i));}
+	}
 
 	@Override
 	public void registerIcons(IconRegister registerer)
-    {
+	{
 		icons = new Icon[14];
-		for(int i = 0; i < 14; i++)
+		for(int i = 0; i < 14; i++) {
 			icons[i] = registerer.registerIcon(Reference.ModID + ":" + textureFolder+MetaNames[i]+" Small Ore");
-    }
-    
-    @Override
+		}
+	}
+
+	@Override
 	public EnumSize getSize() {
 		return EnumSize.TINY;
 	}
-	
+
 	@Override
 	public EnumWeight getWeight() {
 		// TODO Auto-generated method stub
 		return EnumWeight.HEAVY;
+	}
+
+	@Override
+	public short GetMetalReturnAmount(ItemStack is) 
+	{
+		int dam = is.getItemDamage();
+		switch(dam)
+		{
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13: return 10;
+		}
+		return 0;
 	}
 }
