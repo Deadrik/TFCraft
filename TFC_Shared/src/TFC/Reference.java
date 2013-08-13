037d3dd11a0c5e58fb8a0108220018a3b8c5bbc9
Robert Anthony <Bioxx2007@gmail.com>
2013-08-13 08:46:54 -0400

    Changed how TFC is versioned.

--- TFC_Shared/src/TFC/Reference.java
+++ TFC_Shared/src/TFC/Reference.java
@@ -4,7 +4,13 @@ public class Reference
 {
 	public static final String ModID = "terrafirmacraft";
 	public static final String ModName = "TerraFirmaCraft";
-	public static final String ModVersion = "0.77.0";
+
+	public static final int VersionMajor = 0;
+	public static final int VersionMinor = 77;
+	public static final int VersionRevision = 0;
+
+	public static final String ModVersion = VersionMajor+"."+VersionMinor+"."+VersionRevision;
+
 	public static final String ModDependencies = "required-after:Forge@[7.0,);required-after:FML@[5.0.5,)";
 	public static final String ModChannel = "TerraFirmaCraft";
 	public static final String SERVER_PROXY_CLASS = "TFC.CommonProxy";
