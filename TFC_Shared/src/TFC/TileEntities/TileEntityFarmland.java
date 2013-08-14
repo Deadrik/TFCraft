2a8a92b42fe140389cbf520a6b398296073ffd31
Robert Anthony <Bioxx2007@gmail.com>
2013-08-14 11:09:00 -0400

    slight crop tweak to make them make a tad more sense

--- TFC_Shared/src/TFC/TileEntities/TileEntityFarmland.java
+++ TFC_Shared/src/TFC/TileEntities/TileEntityFarmland.java
@@ -18,122 +18,138 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class TileEntityFarmland extends NetworkTileEntity
 {
-    public long nutrientTimer = -1;
-    public int[] nutrients = {18000,18000,18000};
-    
-    /**
-     * Client only
-     * */
-    public long timeSinceUpdate = 0;
+	public long nutrientTimer = -1;
+	public int[] nutrients = {18000,18000,18000};
 
-    public TileEntityFarmland()
-    {
-    	this.shouldSendInitData = false;
-    }
+	/**
+	 * Client only
+	 * */
+	public long timeSinceUpdate = 0;
 
-    @Override
-    public void updateEntity()
-    {
-        if(!worldObj.isRemote)
-        {
-            if(nutrientTimer <= 0)
-                nutrientTimer = TFC_Time.getTotalHours();
-            
-            if(nutrientTimer < TFC_Time.getTotalHours())
-            {
-                CropIndex crop = null;
-                float timeMultiplier = TFC_Time.daysInYear / 360f;
-                int soilMax = (int) (25000 * timeMultiplier);
-                int restoreAmount = 139;
-                
-                if((worldObj.getBlockId(xCoord, yCoord+1, zCoord) == Block.crops.blockID))
-                {
-                    crop = CropManager.getInstance().getCropFromId(((TileEntityCrop)worldObj.getBlockTileEntity(xCoord, yCoord+1, zCoord)).cropId);
-                    
-                    if((crop.cycleType != 0))
-                    {
-                        if(nutrients[0] < soilMax)
-                            nutrients[0] += restoreAmount + crop.nutrientExtraRestore[0];
-                    }
-                    if((crop.cycleType != 1))
-                    {
-                        if(nutrients[1] < soilMax)
-                            nutrients[1] += restoreAmount + crop.nutrientExtraRestore[1];
-                    }
-                    if((crop.cycleType != 2))
-                    {
-                        if(nutrients[2] < soilMax)
-                            nutrients[2] += restoreAmount + crop.nutrientExtraRestore[2];
-                    }
-                }
-                else
-                {
-                    if(nutrients[0] < soilMax)
-                        nutrients[0] += restoreAmount;
-                    if(nutrients[1] < soilMax)
-                        nutrients[1] += restoreAmount;
-                    if(nutrients[2] < soilMax)
-                        nutrients[2] += restoreAmount;
-                }
-                
-                if(nutrients[0] > soilMax)
-                    nutrients[0] = soilMax;
-                if(nutrients[1] > soilMax)
-                    nutrients[1] = soilMax;
-                if(nutrients[2] > soilMax)
-                    nutrients[2] = soilMax;
+	public TileEntityFarmland()
+	{
+		this.shouldSendInitData = false;
+	}
 
-                nutrientTimer+=24;
-                
-//                if(BlockFarmland.isWaterNearby(worldObj, xCoord, yCoord, zCoord))
-//                {
-//                    waterSaturation += 1;
-//                    if(waterSaturation > 30)
-//                        waterSaturation = 30;
-//                }
-//                else if((worldObj.getBlockId(xCoord, yCoord+1, zCoord) == Block.crops.blockID) && crop != null)
-//                {
-//                    waterSaturation -= 1*crop.waterUsageMult;
-//                }
-//                
-//                if(worldObj.isRaining() && worldObj.canBlockSeeTheSky(xCoord, yCoord+1, zCoord))
-//                {
-//                    waterSaturation += 3;
-//                    if(waterSaturation > 30)
-//                        waterSaturation = 30;
-//                }
-            }
-        }
-    }
+	@Override
+	public void updateEntity()
+	{
+		if(!worldObj.isRemote)
+		{
+			if(nutrientTimer <= 0) {
+				nutrientTimer = TFC_Time.getTotalHours();
+			}
 
-    public void DrainNutrients(int type, float multiplier)
-    {
-    	float timeMultiplier = 360f / TFC_Time.daysInYear;
-        nutrients[type] -= (100*multiplier)*timeMultiplier;
-    }
+			if(nutrientTimer < TFC_Time.getTotalHours())
+			{
+				CropIndex crop = null;
 
-    /**
-     * Reads a tile entity from NBT.
-     */
-    @Override
+				int soilMax = getSoilMax();
+				int restoreAmount = 139;
+
+				if((worldObj.getBlockId(xCoord, yCoord+1, zCoord) == Block.crops.blockID))
+				{
+					crop = CropManager.getInstance().getCropFromId(((TileEntityCrop)worldObj.getBlockTileEntity(xCoord, yCoord+1, zCoord)).cropId);
+
+					if((crop.cycleType != 0))
+					{
+						if(nutrients[0] < soilMax) {
+							nutrients[0] += restoreAmount + crop.nutrientExtraRestore[0];
+						}
+					}
+					if((crop.cycleType != 1))
+					{
+						if(nutrients[1] < soilMax) {
+							nutrients[1] += restoreAmount + crop.nutrientExtraRestore[1];
+						}
+					}
+					if((crop.cycleType != 2))
+					{
+						if(nutrients[2] < soilMax) {
+							nutrients[2] += restoreAmount + crop.nutrientExtraRestore[2];
+						}
+					}
+				}
+				else
+				{
+					if(nutrients[0] < soilMax) {
+						nutrients[0] += restoreAmount;
+					}
+					if(nutrients[1] < soilMax) {
+						nutrients[1] += restoreAmount;
+					}
+					if(nutrients[2] < soilMax) {
+						nutrients[2] += restoreAmount;
+					}
+				}
+
+				if(nutrients[0] > soilMax) {
+					nutrients[0] = soilMax;
+				}
+				if(nutrients[1] > soilMax) {
+					nutrients[1] = soilMax;
+				}
+				if(nutrients[2] > soilMax) {
+					nutrients[2] = soilMax;
+				}
+
+				nutrientTimer+=24;
+
+				//                if(BlockFarmland.isWaterNearby(worldObj, xCoord, yCoord, zCoord))
+				//                {
+				//                    waterSaturation += 1;
+				//                    if(waterSaturation > 30)
+				//                        waterSaturation = 30;
+				//                }
+				//                else if((worldObj.getBlockId(xCoord, yCoord+1, zCoord) == Block.crops.blockID) && crop != null)
+				//                {
+				//                    waterSaturation -= 1*crop.waterUsageMult;
+				//                }
+				//                
+				//                if(worldObj.isRaining() && worldObj.canBlockSeeTheSky(xCoord, yCoord+1, zCoord))
+				//                {
+				//                    waterSaturation += 3;
+				//                    if(waterSaturation > 30)
+				//                        waterSaturation = 30;
+				//                }
+			}
+		}
+	}
+
+	public int getSoilMax()
+	{
+		float timeMultiplier = TFC_Time.daysInYear / 360f;
+		return (int) (25000 * timeMultiplier);
+	}
+
+	public void DrainNutrients(int type, float multiplier)
+	{
+		float timeMultiplier = 360f / TFC_Time.daysInYear;
+		nutrients[type] -= (100*multiplier)*timeMultiplier;
+	}
+
+	/**
+	 * Reads a tile entity from NBT.
+	 */
+	@Override
 	public void readFromNBT(NBTTagCompound par1NBTTagCompound)
-    {
-        super.readFromNBT(par1NBTTagCompound);
-        
-        nutrients = par1NBTTagCompound.getIntArray("nutrients");
-        nutrientTimer = par1NBTTagCompound.getLong("nutrientTimer");
-    }
+	{
+		super.readFromNBT(par1NBTTagCompound);
 
-    /**
-     * Writes a tile entity to NBT.
-     */
-    @Override
+		nutrients = par1NBTTagCompound.getIntArray("nutrients");
+		nutrientTimer = par1NBTTagCompound.getLong("nutrientTimer");
+	}
+
+	/**
+	 * Writes a tile entity to NBT.
+	 */
+	@Override
 	public void writeToNBT(NBTTagCompound par1NBTTagCompound)
-    {
-        super.writeToNBT(par1NBTTagCompound);
-        par1NBTTagCompound.setIntArray("nutrients", nutrients);
-        par1NBTTagCompound.setLong("nutrientTimer", nutrientTimer);    
-    }
+	{
+		super.writeToNBT(par1NBTTagCompound);
+		par1NBTTagCompound.setIntArray("nutrients", nutrients);
+		par1NBTTagCompound.setLong("nutrientTimer", nutrientTimer);    
+	}
 
 	@Override
 	public void handleDataPacket(DataInputStream inStream) throws IOException 
@@ -151,16 +167,16 @@ public class TileEntityFarmland extends NetworkTileEntity
 	@Override
 	public void createInitPacket(DataOutputStream outStream) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void handleInitPacket(DataInputStream inStream) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public void requestNutrientData()
 	{
 		if(TFC_Time.getTotalTicks() > timeSinceUpdate + 1000)
@@ -169,7 +185,7 @@ public class TileEntityFarmland extends NetworkTileEntity
 			TerraFirmaCraft.proxy.sendCustomPacket(createNutrientRequestPacket());
 		}
 	}
-	
+
 	public Packet createNutrientRequestPacket()
 	{
 		ByteArrayOutputStream bos=new ByteArrayOutputStream(140);
@@ -185,7 +201,7 @@ public class TileEntityFarmland extends NetworkTileEntity
 
 		return this.setupCustomPacketData(bos.toByteArray(), bos.size());
 	}
-	
+
 	public Packet createNutrientPacket()
 	{
 		ByteArrayOutputStream bos=new ByteArrayOutputStream(140);
