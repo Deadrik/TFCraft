b0af799ee3da5151a32af621f24ff3bef963c17c
Kittychanley <Amanda.D.Halek@gmail.com>
2013-08-15 23:25:47 -0500

    Fixes #218

--- TFC_Shared/src/TFC/Blocks/Terrain/BlockClayGrass.java
+++ TFC_Shared/src/TFC/Blocks/Terrain/BlockClayGrass.java
@@ -2,8 +2,11 @@ package TFC.Blocks.Terrain;
 
 import java.util.Random;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.world.World;
 import TFC.TFCBlocks;
+import TFC.Core.TFC_Core;
 
 public class BlockClayGrass extends BlockGrass
 {
@@ -33,5 +36,24 @@ public class BlockClayGrass extends BlockGrass
 	public int quantityDropped(Random par1Random)
 	{
 		return par1Random.nextInt(4);
+	}
+	
+    @Override
+    public void onNeighborBlockChange(World world, int i, int j, int k, int l)
+    {
+        if(!world.blockExists(i, j-1, k))
+        {
+            int meta = world.getBlockMetadata(i, j, k);
+            world.setBlock(i, j, k, TFC_Core.getTypeForClay(meta), meta, 0x2);
+        }
+    }
+    
+	@Override
+    public void updateTick(World world, int i, int j, int k, Random rand)
+	{
+		if (world.getBlockLightValue(i, j + 1, k) < 4 && Block.lightOpacity[world.getBlockId(i, j + 1, k)] > 2)
+        {
+            world.setBlock(i, j, k, TFC_Core.getTypeForClay(world.getBlockMetadata(i, j, k) + textureOffset), world.getBlockMetadata(i, j, k), 0x2);
+        }
 	}
 }
