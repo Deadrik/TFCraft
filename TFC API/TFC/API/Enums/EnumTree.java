845b21b5c691d0aeff424cf97380f2359688c728
Duncan <duncan.britton@outlook.com>
2013-08-18 14:22:55 -0400

    More edits to trees to better reflect normal growing conditions.

--- TFC API/TFC/API/Enums/EnumTree.java
+++ TFC API/TFC/API/Enums/EnumTree.java
@@ -2,21 +2,21 @@ package TFC.API.Enums;
 
 public enum EnumTree
 {
-	OAK("OAK", /*minRain*/400f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/24, /*minEVT*/0.25f, /*maxEVT*/2, false),
+	OAK("OAK", /*minRain*/400f, /*maxRain*/16000f, /*minTemp*/4, /*maxTemp*/24, /*minEVT*/0.25f, /*maxEVT*/2, false),
 	
-	ASPEN("ASPEN", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, false),
+	ASPEN("ASPEN", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/2, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, false),
 	
-	BIRCH("BIRCH", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, false),
+	BIRCH("BIRCH", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/2, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, false),
 	
 	CHESTNUT("CHESTNUT", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, false),
 	
-	DOUGLASFIR("DOUGLASFIR", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, true),
+	DOUGLASFIR("DOUGLASFIR", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/1, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, true),
 	
-	HICKORY("HICKORY", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, false),
+	HICKORY("HICKORY", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/4, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, false),
 	
 	MAPLE("MAPLE", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/12, /*minEVT*/0, /*maxEVT*/1, false),
 	
-	ASH("ASH", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/2, false),
+	ASH("ASH", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/4, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/2, false),
 	
 	PINE("PINE", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/1, /*maxTemp*/14, /*minEVT*/0, /*maxEVT*/1, true),
 	
@@ -26,9 +26,9 @@ public enum EnumTree
 	
 	SYCAMORE("SYCAMORE", /*minRain*/400f, /*maxRain*/16000f, /*minTemp*/6, /*maxTemp*/30, /*minEVT*/0, /*maxEVT*/1, false),
 	
-	WHITECEDAR("WHITECEDAR", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/1, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, true),
+	WHITECEDAR("WHITECEDAR", /*minRain*/250f, /*maxRain*/16000f, /*minTemp*/2, /*maxTemp*/24, /*minEVT*/0, /*maxEVT*/1, true),
 	
-	WHITEELM("WHITEELM", /*minRain*/400f, /*maxRain*/16000f, /*minTemp*/3, /*maxTemp*/30, /*minEVT*/0, /*maxEVT*/1, false),
+	WHITEELM("WHITEELM", /*minRain*/400f, /*maxRain*/16000f, /*minTemp*/4, /*maxTemp*/30, /*minEVT*/0, /*maxEVT*/1, false),
 	
 	WILLOW("WILLOW", /*minRain*/1000f, /*maxRain*/16000f, /*minTemp*/6, /*maxTemp*/30, /*minEVT*/0, /*maxEVT*/1, false),
 	
