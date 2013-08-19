8447c912c762ab474b8484fe58727555385841b2
Robert Anthony <Bioxx2007@gmail.com>
2013-08-19 13:18:45 -0400

    Greatly lowered the speed of the skeletons. For whatever reason a speed of 1.0 which is what vanilla skeletons use, causes our skeletons to gain super speed.

--- TFC_Shared/src/TFC/Entities/Mobs/EntitySkeletonTFC.java
+++ TFC_Shared/src/TFC/Entities/Mobs/EntitySkeletonTFC.java
@@ -50,7 +50,7 @@ public class EntitySkeletonTFC extends EntityMob implements IRangedAttackMob, IC
 		this.tasks.addTask(1, new EntityAISwimming(this));
 		this.tasks.addTask(2, new EntityAIRestrictSun(this));
 		this.tasks.addTask(3, new EntityAIFleeSun(this, 1.0D));
-		this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(5, new EntityAIWander(this, 0.4D));
 		this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
 		this.tasks.addTask(6, new EntityAILookIdle(this));
 		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
