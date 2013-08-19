966c796bdaff70d7c2a7026116b9929d23316b8b
Robert Anthony <Bioxx2007@gmail.com>
2013-08-19 19:54:47 -0400

    Jugs are no longer stackable.

--- TFC_Shared/src/TFC/Items/Pottery/ItemPotteryJug.java
+++ TFC_Shared/src/TFC/Items/Pottery/ItemPotteryJug.java
@@ -26,6 +26,7 @@ public class ItemPotteryJug extends ItemPotteryBase
 	{
 		super(id);
 		this.MetaNames = new String[]{"Clay Jug", "Ceramic Jug", "Water Jug"};
+		this.stackable = false;
 	}
 
 	@Override
