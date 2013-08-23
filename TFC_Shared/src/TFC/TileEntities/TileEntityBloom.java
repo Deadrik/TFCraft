b3c7b411f7f7462e6a0fb12428f32f1278e76c9f
Robert Anthony <Bioxx2007@gmail.com>
2013-08-23 15:17:41 -0400

    Bloom blocks should no longer lose their iron amount information if unloaded. No more 0% bloom items.

--- TFC_Shared/src/TFC/TileEntities/TileEntityBloom.java
+++ TFC_Shared/src/TFC/TileEntities/TileEntityBloom.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import net.minecraft.nbt.NBTTagCompound;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -20,28 +21,42 @@ public class TileEntityBloom extends NetworkTileEntity
 	}
 
 	@Override
+	public void writeToNBT(NBTTagCompound nbttagcompound)
+	{
+		super.writeToNBT(nbttagcompound);
+		nbttagcompound.setInteger("size", size);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbttagcompound)
+	{
+		super.readFromNBT(nbttagcompound);
+		size = nbttagcompound.getInteger("size");
+	}
+
+	@Override
 	public void handleDataPacket(DataInputStream inStream) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void handleDataPacketServer(DataInputStream inStream)
 			throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void createInitPacket(DataOutputStream outStream) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void handleInitPacket(DataInputStream inStream) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
