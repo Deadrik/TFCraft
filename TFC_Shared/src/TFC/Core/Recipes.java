dc15c8381fdf85a4f591f85419400031c225c113
Kittychanley <Amanda.D.Halek@gmail.com>
2013-08-24 16:44:25 -0500

    Adds Wool Yarn Recipes for the Fishing Rod

--- TFC_Shared/src/TFC/Core/Recipes.java
+++ TFC_Shared/src/TFC/Core/Recipes.java
@@ -237,6 +237,9 @@ public class Recipes
 		GameRegistry.addRecipe(new ItemStack(Item.bow,1), new Object[] {" #$","# $"," #$",Character.valueOf('#'),new ItemStack(Item.stick,1),Character.valueOf('$'),new ItemStack(TFCItems.WoolYarn,1)});
 		GameRegistry.addRecipe(new ItemStack(Item.bow,1), new Object[] {"$# ","$ #","$# ",Character.valueOf('#'),new ItemStack(Item.stick,1),Character.valueOf('$'),new ItemStack(TFCItems.WoolYarn,1)});
 
+		GameRegistry.addRecipe(new ItemStack(Item.fishingRod,1), new Object[] {"  #"," #$","# $",Character.valueOf('#'),new ItemStack(Item.stick,1),Character.valueOf('$'),new ItemStack(TFCItems.WoolYarn,1)});
+		GameRegistry.addRecipe(new ItemStack(Item.fishingRod,1), new Object[] {"#  ","$# ","$ #",Character.valueOf('#'),new ItemStack(Item.stick,1),Character.valueOf('$'),new ItemStack(TFCItems.WoolYarn,1)});
+
 		//Tool Rack & Barrel
 		for(int j = 0; j < 16; j++)
 		{
