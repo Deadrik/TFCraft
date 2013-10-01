7cdfe9636db1a8289457ca8f24cf9e994a4d68ee
Robert Anthony <Bioxx2007@gmail.com>
2013-10-01 08:40:08 -0400

    Updated release scripts so that they now accept a version number as part of the input arguments. The release batch file now asks for the version number and feeds it to the python scripts.

--- release.bat
+++ release.bat
@@ -1,3 +1,10 @@
 @echo off
-..\runtime\bin\python\python_mcp release.py %*
+
+set major_num=
+set /p major_num=Please Enter the Major Number:
+set build_num=
+set /p build_num=Please Enter the Build Number:
+set revision_num=
+set /p revision_num=Please Enter the Revision Number:
+..\runtime\bin\python\python_mcp release.py %major_num% %build_num% %revision_num%
 pause
