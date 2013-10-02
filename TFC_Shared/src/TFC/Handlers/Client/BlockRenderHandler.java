fa6de90efba649a18ce63b055ff24666b87c7395
Robert Anthony <Bioxx2007@gmail.com>
2013-10-02 05:39:38 -0400

    Wood Construct Renderer isolated

--- TFC_Shared/src/TFC/Handlers/Client/BlockRenderHandler.java
+++ TFC_Shared/src/TFC/Handlers/Client/BlockRenderHandler.java
@@ -1,7 +1,6 @@
 package TFC.Handlers.Client;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockFence;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.Icon;
@@ -17,7 +16,6 @@ import TFC.Render.Blocks.RenderDetailed;
 import TFC.Render.Blocks.RenderFluids;
 import TFC.Render.Blocks.RenderFoodPrep;
 import TFC.Render.Blocks.RenderGrass;
-import TFC.Render.Blocks.RenderWoodConstruct;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
 public class BlockRenderHandler implements ISimpleBlockRenderingHandler 
@@ -27,46 +25,46 @@ public class BlockRenderHandler implements ISimpleBlockRenderingHandler
 			Block block, int modelId, RenderBlocks renderer) 
 	{
 		if (modelId == TFCBlocks.sulfurRenderId)
-        {
-            return TFC_CoreRender.RenderSulfur(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.moltenRenderId)
-        {
-            return TFC_CoreRender.RenderMolten(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.grassRenderId)
-        {
-            return RenderGrass.render(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.clayGrassRenderId)
-        {
-        	return RenderGrass.renderClay(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.peatGrassRenderId)
-        {
-        	return RenderGrass.renderPeat(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.looseRockRenderId)
-        {
-            return TFC_CoreRender.RenderLooseRock(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.snowRenderId)
-        {
-            return TFC_CoreRender.RenderSnow(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.FirepitRenderId)
-        {
-            return TFC_CoreRender.renderFirepit(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.ForgeRenderId)
-        {
-            return TFC_CoreRender.renderForge(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.AnvilRenderId)
-        {
-            return RenderAnvil.renderAnvil(block, i, j, k, renderer);
-        }
-       /* else if (modelId == TFCBlocks.IngotPileRenderId)
+		{
+			return TFC_CoreRender.RenderSulfur(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.moltenRenderId)
+		{
+			return TFC_CoreRender.RenderMolten(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.grassRenderId)
+		{
+			return RenderGrass.render(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.clayGrassRenderId)
+		{
+			return RenderGrass.renderClay(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.peatGrassRenderId)
+		{
+			return RenderGrass.renderPeat(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.looseRockRenderId)
+		{
+			return TFC_CoreRender.RenderLooseRock(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.snowRenderId)
+		{
+			return TFC_CoreRender.RenderSnow(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.FirepitRenderId)
+		{
+			return TFC_CoreRender.renderFirepit(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.ForgeRenderId)
+		{
+			return TFC_CoreRender.renderForge(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.AnvilRenderId)
+		{
+			return RenderAnvil.renderAnvil(block, i, j, k, renderer);
+		}
+		/* else if (modelId == TFCBlocks.IngotPileRenderId)
         {
         	if(((BlockIngotPile)block).stack < 10){
         		System.out.println(((BlockIngotPile)block).stack+" is the stack");
@@ -74,35 +72,35 @@ public class BlockRenderHandler implements ISimpleBlockRenderingHandler
         	}
         	return TFC_CoreRender.RenderSluice(block, i, j, k, renderer);
         }*/
-        else if (modelId == TFCBlocks.sluiceRenderId)
-        {
-            return TFC_CoreRender.RenderSluice(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.woodFruitRenderId)
-        {
-            return TFC_CoreRender.RenderWoodTrunk(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.leavesFruitRenderId)
-        {
-            return TFC_CoreRender.RenderFruitLeaves(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.finiteWaterRenderId)
-        {
-            return TFC_CoreRender.RenderFiniteWater(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.stairRenderId)
-        {
-            return TFC_CoreRender.renderBlockStairs(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.slabRenderId)
-        {
-            return TFC_CoreRender.renderBlockSlab(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.cropRenderId)
-        {
-            return RenderCrop.render(block, i, j, k, renderer);
-        }
-        /*else if (modelId == TFCBlocks.leavesRenderId)
+		else if (modelId == TFCBlocks.sluiceRenderId)
+		{
+			return TFC_CoreRender.RenderSluice(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.woodFruitRenderId)
+		{
+			return TFC_CoreRender.RenderWoodTrunk(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.leavesFruitRenderId)
+		{
+			return TFC_CoreRender.RenderFruitLeaves(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.finiteWaterRenderId)
+		{
+			return TFC_CoreRender.RenderFiniteWater(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.stairRenderId)
+		{
+			return TFC_CoreRender.renderBlockStairs(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.slabRenderId)
+		{
+			return TFC_CoreRender.renderBlockSlab(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.cropRenderId)
+		{
+			return RenderCrop.render(block, i, j, k, renderer);
+		}
+		/*else if (modelId == TFCBlocks.leavesRenderId)
         {
             int var5 = block.colorMultiplier(world, i, j, k);
             float var6 = (float)(var5 >> 16 & 255) / 255.0F;
@@ -110,22 +108,18 @@ public class BlockRenderHandler implements ISimpleBlockRenderingHandler
             float var8 = (float)(var5 & 255) / 255.0F;
             return RenderLeaves.renderLeaves(block, i, j, k, var6, var7, var8, (RenderBlocks)renderer, ModLoader.getMinecraftInstance().isFancyGraphicsEnabled(), true);
         }*/
-        else if (modelId == TFCBlocks.detailedRenderId)
-        {
-            return RenderDetailed.renderBlockDetailed(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.foodPrepRenderId)
-        {
-            return RenderFoodPrep.render(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.fluidRenderId)
-        {
-            return RenderFluids.render(block, i, j, k, renderer);
-        }
-        else if (modelId == TFCBlocks.woodConstructRenderId)
-        {
-            return RenderWoodConstruct.render(block, i, j, k, renderer);
-        }
+		else if (modelId == TFCBlocks.detailedRenderId)
+		{
+			return RenderDetailed.renderBlockDetailed(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.foodPrepRenderId)
+		{
+			return RenderFoodPrep.render(block, i, j, k, renderer);
+		}
+		else if (modelId == TFCBlocks.fluidRenderId)
+		{
+			return RenderFluids.render(block, i, j, k, renderer);
+		}
 		return false;
 	}
 
@@ -144,7 +138,7 @@ public class BlockRenderHandler implements ISimpleBlockRenderingHandler
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer) {
 		Icon[] icons = new Icon[6];
-		
+
 		if (modelID == TFCBlocks.peatGrassRenderId) {
 			for(int i = 0; i < 6; i++) {
 				icons[i] = TFCBlocks.Peat.getBlockTextureFromSide(i);
