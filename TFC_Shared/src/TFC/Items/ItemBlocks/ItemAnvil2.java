fbb1a755a1f54b9dfbbe32f0c2e43b8715faf2d4
dunkleosteus <nikstick22@GMAIL.COM>
2013-10-04 19:53:44 -0400

    ItemAnvil2 now also melts in a crucible

--- TFC_Shared/src/TFC/Items/ItemBlocks/ItemAnvil2.java
+++ TFC_Shared/src/TFC/Items/ItemBlocks/ItemAnvil2.java
@@ -1,10 +1,14 @@
 package TFC.Items.ItemBlocks;
 
 import net.minecraft.item.ItemStack;
+import TFC.API.ISmeltable;
+import TFC.API.Metal;
+import TFC.API.Constant.Global;
 import TFC.API.Enums.EnumSize;
 import TFC.API.Enums.EnumWeight;
+import TFC.API.ISmeltable.EnumTier;
 
-public class ItemAnvil2 extends ItemTerraBlock
+public class ItemAnvil2 extends ItemTerraBlock implements ISmeltable
 {
 	public ItemAnvil2(int i) 
 	{
@@ -20,5 +24,36 @@ public class ItemAnvil2 extends ItemTerraBlock
 	@Override
 	public EnumWeight getWeight(ItemStack is) {
 		return EnumWeight.HEAVY;
+	}
+	
+	@Override
+	public Metal GetMetalType(ItemStack is) 
+	{
+		int meta = is.getItemDamage();
+		switch(meta)
+		{
+		case 0: return Global.ROSEGOLD;
+		case 1: return Global.BISMUTHBRONZE;
+		case 2: return Global.BLACKBRONZE;
+		default : return Global.UNKNOWN;
+		}
+	}
+	
+	@Override
+	public short GetMetalReturnAmount(ItemStack is) {
+
+		return 1400;
+	}
+
+	@Override
+	public boolean isSmeltable(ItemStack is) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public EnumTier GetSmeltTier(ItemStack is) {
+		// TODO Auto-generated method stub
+		return EnumTier.TierI;
 	}
 }
