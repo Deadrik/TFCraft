06f83a963124b782e44c17b66b8d54d50475ce07
dunkleosteus <nikstick22@GMAIL.COM>
2014-04-06 13:59:22 -0400

    Fixed armor right click problem

--- TFC_Shared/src/TFC/Items/ItemTFCArmor.java
+++ TFC_Shared/src/TFC/Items/ItemTFCArmor.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.ItemArmor;
@@ -11,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.Icon;
+import net.minecraft.world.World;
 import TFC.Reference;
 import TFC.API.Armor;
 import TFC.API.IClothing;
@@ -86,6 +88,24 @@ public class ItemTFCArmor extends ItemArmor implements ISize, IClothing
 		return EnumSize.LARGE;
 	}
 
+	@Override
+	/**
+     * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
+     */
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        int i = EntityLiving.getArmorPosition(par1ItemStack);
+        ItemStack itemstack1 = par3EntityPlayer.getCurrentArmor(i);
+
+        if (itemstack1 == null)
+        {
+            par3EntityPlayer.setCurrentItemOrArmor(i + 1, par1ItemStack.copy()); //Forge: Vanilla bug fix associated with fixed setCurrentItemOrArmor indexs for players.
+            par1ItemStack.stackSize = 0;
+        }
+
+        return par1ItemStack;
+    }
+	
 	@Override
 	public boolean canStack() 
 	{
