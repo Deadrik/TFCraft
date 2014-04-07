9b3ec4214b4e90ff4baabbbdeff5373cfae9250b
dunkleosteus <nikstick22@GMAIL.COM>
2014-04-07 11:23:57 -0400

    SMP-compatible fix. Should be able to catch the exception and try again if it fails

--- TFC_Shared/src/TFC/Core/TFC_Achievements.java
+++ TFC_Shared/src/TFC/Core/TFC_Achievements.java
@@ -21,34 +21,34 @@ public class TFC_Achievements {
 	public static Achievement achDiamond;
 	public static Achievement achAnvil;
 	public static Achievement achQuern;
-	
+
 	private static int achievementIndex = 1000;
 	private static ArrayList<Achievement> achlist;
-	
+
 	public static AchievementPage pageBiome;
-	
+
 	static Achievement[] achievementsTFC;
-	
+
 	public TFC_Achievements(){
 		init();
 	}
-	
+
 	public static void init()
 	{
 		achlist = new ArrayList<Achievement>();
-		
+
 		achLooseRock = createAchievement("TFC.achLooseRock",0,0,new ItemStack(TFCItems.LooseRock,1,0), null, "Definitely a rock", "Pick up a rock");
-		
+
 		achSmallOre = createAchievement("TFC.achSmallOre",2,0,new ItemStack(TFCItems.SmallOreChunk,1,0), achLooseRock, "No stone unturned", "Find an ore nugget");
-				
+
 		achWildVegetable = createAchievement("TFC.achWildVegetable",0,-5,new ItemStack(TFCItems.Onion),null,"Gatherer","Find a wild vegetable");
-		
+
 		achRutabaga = createAchievement("TFC.achRutabaga",-1,-5,new ItemStack(TFCItems.Onion,1,1),achWildVegetable,"Stupid onions...","Find a rutabaga");
-		
+
 		achDiamond = createAchievement("TFC.achDiamond",0,5,new ItemStack(TFCItems.GemDiamond,1,4),null,"DIAM- oh wait, no","Find a diamond");
-		
+
 		achAnvil = createAchievement("TFC.achAnvil",2,-2, new ItemStack(TFCBlocks.Anvil,1,1),null,"Hammer Time","Make an anvil");
-		
+
 		achievementsTFC = new Achievement[achlist.size()];
 		achievementsTFC = achlist.toArray(achievementsTFC);
 
@@ -56,19 +56,29 @@ public class TFC_Achievements {
 
 		AchievementPage.registerAchievementPage(pageBiome);
 	}
-	
+
 	private static Achievement createAchievement(String name, int posX, int posY, ItemStack is, Achievement preReq, String IGN,String desc){
-		Achievement a = (new Achievement(achievementIndex++, name,posX,posY,is,preReq)).registerAchievement();
+		boolean fail = true;
+		Achievement a = null;
+		while(fail){
+			try{
+				fail = false;
+				a = (new Achievement(achievementIndex++, name,posX,posY,is,preReq)).registerAchievement();
+			}
+			catch(RuntimeException re){
+				fail = true;
+			}
+		}
 		addAchievementDesc(name,IGN, desc);
 		achlist.add(a);
 		return a;
 	}
-	
+
 	@ForgeSubscribe
 	public void chunkEntered(EntityEvent.EnteringChunk event)
 	{
 	}
-	
+
 	private static void addAchievementDesc(String ach, String name, String desc)
 	{
 		LanguageRegistry.instance().addStringLocalization("achievement." + ach, "en_US", name);
