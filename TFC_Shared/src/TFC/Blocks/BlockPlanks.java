60f2c3ce4cdc121bf2a428ab9a0764bbc974a476
Bioxx <bioxx2007@gmail.com>
2014-04-08 20:58:46 -0400

    Reverted the BlockWood missing texture fix. We'll just live with that for now

--- TFC_Shared/src/TFC/Blocks/BlockPlanks.java
+++ TFC_Shared/src/TFC/Blocks/BlockPlanks.java
@@ -2,6 +2,7 @@ package TFC.Blocks;
 
 import java.util.List;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
@@ -63,6 +64,7 @@ public class BlockPlanks extends BlockTerra
 	{
 		for(int i = 0; i < woodNames.length; i++)
 			icons[i] = registerer.registerIcon(Reference.ModID + ":" + "wood/"+woodNames[i]+" Plank");
+		Block.planks.registerIcons(registerer);
 	}
 
 	/**
