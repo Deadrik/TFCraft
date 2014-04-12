b96d7f716c4e2a30001829dd661f76a408da746e
Bioxx <bioxx2007@gmail.com>
2014-04-12 02:09:18 -0400

    Fixed divide by zero in WorldGenGrowCrops

--- TFC_Shared/src/TFC/WorldGen/Generators/WorldGenGrowCrops.java
+++ TFC_Shared/src/TFC/WorldGen/Generators/WorldGenGrowCrops.java
@@ -47,7 +47,8 @@ public class WorldGenGrowCrops implements IWorldGenerator
 						world.setBlock(i, j, k, Block.crops.blockID, 0, 0x2);
 						te = (TECrop)world.getBlockTileEntity(i, j, k);
 						te.cropId = cropBlockId;
-						float mg = Math.min(month/(crop.growthTime/TFC_Time.daysInMonth), 1.0f)*(0.75f+(rand.nextFloat()*0.25f));
+						float gt = Math.max(crop.growthTime/TFC_Time.daysInMonth, 0.01f);
+						float mg = Math.min(month/gt, 1.0f)*(0.75f+(rand.nextFloat()*0.25f));
 						float growth =  Math.min(crop.numGrowthStages*mg, crop.numGrowthStages);
 						te.growth = growth;
 					}
