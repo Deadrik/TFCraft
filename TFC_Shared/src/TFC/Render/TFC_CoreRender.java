81bcc9b6bd6a0fc7e07416c7daefd7f0363a3210
Bioxx <bioxx2007@gmail.com>
2014-04-25 23:48:52 -0400

    Fixed server crash due to new ASM stuff

--- TFC_Shared/src/TFC/Render/TFC_CoreRender.java
+++ TFC_Shared/src/TFC/Render/TFC_CoreRender.java
@@ -1,14 +1,18 @@
 package TFC.Render;
 
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.src.ModLoader;
 import net.minecraft.util.Icon;
 import net.minecraft.world.IBlockAccess;
@@ -17,17 +21,16 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 import org.lwjgl.opengl.GL11;
 
+import TFC.TFCASMLoadingPlugin;
 import TFC.TFCBlocks;
+import TFC.TerraFirmaCraftCore;
 import TFC.API.TFCOptions;
+import TFC.API.Util.Helper;
 import TFC.Blocks.Devices.BlockSluice;
 import TFC.Blocks.Flora.BlockFruitLeaves;
-import TFC.Blocks.Flora.BlockTallSeaGrassStill;
 import TFC.Core.TFC_Time;
 import TFC.Food.FloraIndex;
 import TFC.Food.FloraManager;
-import TFC.Render.Models.ModelCookingPot;
-import TFC.Render.Models.ModelPotteryBase;
-import TFC.Render.Models.ModelRendererTFC;
 import TFC.TileEntities.TileEntityFruitTreeWood;
 import TFC.TileEntities.TileEntityPartial;
 import TFC.WorldGen.DataLayer;
@@ -299,10 +302,6 @@ public class TFC_CoreRender
 
 		return true;
 	}
-
-
-
-
 
 	public static boolean RenderOre(Block block, int xCoord, int yCoord, int zCoord,float par5, float par6, float par7, RenderBlocks renderblocks, IBlockAccess iblockaccess)
 	{
@@ -1040,5 +1039,50 @@ public class TFC_CoreRender
 		var9.addVertexWithUV(xMax, y + 0.0D, zMin, minX, maxY);
 		var9.addVertexWithUV(xMin, y + 0.0D, zMax, maxX, maxY);
 		var9.addVertexWithUV(xMin, y + 0, zMax, maxX, minY);
+	}
+
+	public static void loadRenderers()
+	{
+		if(!TerraFirmaCraftCore.proxy.isRemote())
+		{
+			return;
+		}
+		RenderGlobal renderG = Minecraft.getMinecraft().renderGlobal;
+		int k = 0, j = 0;
+		int renderChunksWide = Helper.getInteger(renderG, "w", "renderChunksWide", TFCASMLoadingPlugin.runtimeDeobf);
+		int renderChunksTall = Helper.getInteger(renderG, "x", "renderChunksTall", TFCASMLoadingPlugin.runtimeDeobf);
+		int renderChunksDeep = Helper.getInteger(renderG, "y", "renderChunksDeep", TFCASMLoadingPlugin.runtimeDeobf);
+		int glRenderListBase = Helper.getInteger(renderG, "z", "glRenderListBase", TFCASMLoadingPlugin.runtimeDeobf);
+		WorldRenderer[] worldRenderers = (WorldRenderer[])(Helper.getObject(renderG, "v", "worldRenderers", TFCASMLoadingPlugin.runtimeDeobf));
+		WorldRenderer[] sortedWorldRenderers = (WorldRenderer[])(Helper.getObject(renderG, "u", "sortedWorldRenderers", TFCASMLoadingPlugin.runtimeDeobf));
+		List worldRenderersToUpdate = (ArrayList)(Helper.getObject(renderG, "t", "worldRenderersToUpdate", TFCASMLoadingPlugin.runtimeDeobf));
+
+		worldRenderersToUpdate.clear();
+		renderG.tileEntities.clear();
+
+		for (int l = 0; l < renderChunksWide; ++l)
+		{
+			for (int i1 = renderChunksTall-1; i1 >= 0; --i1)
+			{
+				for (int j1 = 0; j1 < renderChunksDeep; ++j1)
+				{
+					worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l] = new WorldRenderer(renderG.theWorld, renderG.tileEntities, l * 16, i1 * 16, j1 * 16, glRenderListBase + j);
+
+					if (Helper.getBoolean(renderG, "D", "occlusionEnabled", TFCASMLoadingPlugin.runtimeDeobf))
+					{
+						worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l].glOcclusionQuery = ((IntBuffer)(Helper.getObject(renderG, "C", "glOcclusionQueryBase", TFCASMLoadingPlugin.runtimeDeobf))).get(k);
+					}
+
+					worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l].isWaitingOnOcclusionQuery = false;
+					worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l].isVisible = true;
+					worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l].isInFrustum = true;
+					worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l].chunkIndex = k++;
+					worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l].markDirty();
+					sortedWorldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l] = worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l];
+					worldRenderersToUpdate.add(worldRenderers[(j1 * renderChunksTall + i1) * renderChunksWide + l]);
+					j += 3;
+				}
+			}
+		}
 	}
 }
