244c11175be436739ef666a1575efc5980356826
ntwitch <ntwitch@gmail.com>
2014-05-02 20:25:27 -0500

    Revert "Gold pan drops nugget item on success, so that success is more obvious."
    This reverts commit 0bc6fdc472c86ebb6d383bcea63da755353c2e83.
    
    Conflicts:
    	TFC_Shared/src/TFC/Items/Tools/ItemGoldPan.java

--- TFC_Shared/src/TFC/Items/Tools/ItemGoldPan.java
+++ TFC_Shared/src/TFC/Items/Tools/ItemGoldPan.java
@@ -5,7 +5,6 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumMovingObjectType;
