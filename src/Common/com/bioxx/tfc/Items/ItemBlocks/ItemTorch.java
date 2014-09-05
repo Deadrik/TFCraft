cb19a2f36e5501d89cc4fb4710a4482ae2006e86
Bioxx <bioxx2007@gmail.com>
2014-09-05 11:15:55 -0400

    Added a timer check to the torch item so that the player has 8 seconds to pick up a torch item from on top of a log pile before it initiates burn.

--- src/Common/com/bioxx/tfc/Items/ItemBlocks/ItemTorch.java
+++ src/Common/com/bioxx/tfc/Items/ItemBlocks/ItemTorch.java
@@ -18,10 +18,15 @@ public class ItemTorch extends ItemTerraBlock
 	{
 		if(entityItem.worldObj.getBlock((int)Math.floor(entityItem.posX), (int)Math.floor(entityItem.posY)-1, (int)Math.floor(entityItem.posZ)) == TFCBlocks.LogPile)
 		{
-			TELogPile te = (TELogPile) entityItem.worldObj.getTileEntity((int)Math.floor(entityItem.posX), (int)Math.floor(entityItem.posY)-1, (int)Math.floor(entityItem.posZ));
-			te.activateCharcoal();
-			te.neighborChanged();
-			entityItem.setDead();
+			int count = entityItem.getEntityData().getInteger("torchCount");
+			if(count > 160)
+			{
+				TELogPile te = (TELogPile) entityItem.worldObj.getTileEntity((int)Math.floor(entityItem.posX), (int)Math.floor(entityItem.posY)-1, (int)Math.floor(entityItem.posZ));
+				te.activateCharcoal();
+				te.neighborChanged();
+				entityItem.setDead();
+			}
+			else entityItem.getEntityData().setInteger("torchCount", count+1);
 		}
 		return false;
 	}
