0bbe7c47db480de95b68ce009a99b8f39207ffcd
Bioxx <bioxx2007@gmail.com>
2014-09-18 22:11:48 -0400

    Fixed the damage tick issue when the player is being healed. It's odd that this alone was the fix but SSP and SMP testing looked like normal damage animations sill play just fine.

---
+++ src/ASM/com/bioxx/tfc/ASM/Transform/TF_EntityPlayerMP.java
@@ -0,0 +1,34 @@
+package com.bioxx.tfc.ASM.Transform;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.LineNumberNode;
+
+import com.bioxx.tfc.ASM.ClassTransformer;
+
+public class TF_EntityPlayerMP extends ClassTransformer
+{
+
+	public TF_EntityPlayerMP()
+	{
+		mcpClassName = "net.minecraft.entity.player.EntityPlayerMP";
+		obfClassName = "mw";
+
+		/**
+		 * This removes the "this.getHealth() != this.lastHealth" part of the if statement
+		 */
+		List<InstrSet> nodes = new ArrayList<InstrSet>();
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 59, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 60, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 61, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 62, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 63, InstrOpType.Remove));
+		nodes.add(new InstrSet(new LineNumberNode(0, new LabelNode()), 64, InstrOpType.Remove));
+
+		mcpMethodNodes.put("onUpdateEntity | ()V", new Patch(nodes, PatchOpType.Modify));
+		obfMethodNodes.put("h | ()V", new Patch(nodes, PatchOpType.Modify));
+	}
+
+}
