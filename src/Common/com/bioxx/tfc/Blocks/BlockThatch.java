fc504677b83bd8750ab3ade541bebcc6ea8c1931
Bioxx <bioxx2007@gmail.com>
2014-10-03 21:07:20 -0400

    Nerfed thatch. It is no longer a solid block. Can not be placed on soil, or on top of other thatch

--- src/Common/com/bioxx/tfc/Blocks/BlockThatch.java
+++ src/Common/com/bioxx/tfc/Blocks/BlockThatch.java
@@ -4,18 +4,17 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.particle.EffectRenderer;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.world.Explosion;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import com.bioxx.tfc.Reference;
 import com.bioxx.tfc.Core.TFCTabs;
+import com.bioxx.tfc.Core.TFC_Core;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -45,5 +44,57 @@ public class BlockThatch extends BlockTerra
 
 		//super.harvestBlock(world, entityplayer, i, j, k, l);
 		dropBlockAsItem(world, i, j, k, new ItemStack(this, 1, l));
+	}
+
+	@Override
+	public boolean getBlocksMovement(IBlockAccess bAccess, int x, int y, int z)
+	{
+		return true;
+	}
+
+	@Override
+	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity)
+	{
+		entity.motionX *= 0.8D;
+		entity.motionZ *= 0.8D;
+	}
+
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z)
+	{
+		return null;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockAccess world, int x, int y, int z, int side)
+	{
+		Block block = world.getBlock(x, y, z);
+		return side == 0 && this.minY > 0.0D ? true : (side == 1 && this.maxY < 1.0D ? true : (side == 2 && this.minZ > 0.0D ? true : (side == 3 && this.maxZ < 1.0D ? true : (side == 4 && this.minX > 0.0D ? true : (side == 5 && this.maxX < 1.0D ? true : !world.getBlock(x, y, z).isOpaqueCube())))));
+	}
+
+	@Override
+	public boolean canPlaceBlockAt(World world, int x, int y, int z)
+	{
+		return canBlockStay(world, x, y, z) && super.canPlaceBlockAt(world, x, y, z);
+	}
+
+	@Override
+	public boolean canBlockStay(World world, int x, int y, int z)
+	{
+		Block id = world.getBlock(x, y - 1, z);
+		return y > 0 && y < 256 ? !TFC_Core.isGround(id) && id != this : false;
+	}
+
+	@Override
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean renderAsNormalBlock()
+	{
+		return false;
 	}
 }
