dcb83d1389ba8f1d9f53c7cadb8a1de10604a10a
Kittychanley <Kittychanley@gmail.com>
2014-10-10 22:14:52 -0500

    Fixed vinegar recipe to be 10oz fruit per bucket instead of 10.01 oz.

--- src/API/com/bioxx/tfc/api/Crafting/BarrelVinegarRecipe.java
+++ src/API/com/bioxx/tfc/api/Crafting/BarrelVinegarRecipe.java
@@ -20,7 +20,7 @@ public class BarrelVinegarRecipe extends BarrelRecipe
 	{
 		if(item.getItem() instanceof IFood)
 		{
-			if(fluid.isFluidEqual(barrelFluid) && ((IFood)item.getItem()).getFoodGroup() == EnumFoodGroup.Fruit && ((IFood)item.getItem()).getFoodWeight(item) > 1f*(fluid.amount/100))
+			if (fluid.isFluidEqual(barrelFluid) && ((IFood) item.getItem()).getFoodGroup() == EnumFoodGroup.Fruit && ((IFood) item.getItem()).getFoodWeight(item) >= 1f * (fluid.amount / 100))
 			{
 				return true;
 			}
