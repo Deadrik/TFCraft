0179c47fcb8256dde6396f7c707fd8551b307918
CarlosHeuberger <fursgyv.cfh@dfgh.net>
2014-10-13 14:17:38 +0000

    only decrease familiarity once a day

--- src/Common/com/bioxx/tfc/Entities/Mobs/EntityCowTFC.java
+++ src/Common/com/bioxx/tfc/Entities/Mobs/EntityCowTFC.java
@@ -633,9 +633,10 @@ public class EntityCowTFC extends EntityCow implements IAnimal
 
 	@Override
 	public void handleFamiliarityUpdate() {
-		if(lastFamiliarityUpdate < TFC_Time.getTotalDays()){
+		int totalDays = TFC_Time.getTotalDays();
+		if(lastFamiliarityUpdate < totalDays){
 			if(familiarizedToday && familiarity < 100){
-				lastFamiliarityUpdate = TFC_Time.getTotalDays();
+				lastFamiliarityUpdate = totalDays;
 				familiarizedToday = false;
 				float familiarityChange = (6 * obedience_mod / aggression_mod);
 				if(this.isAdult() && (familiarity > 30 && familiarity < 80)){
@@ -654,7 +655,7 @@ public class EntityCowTFC extends EntityCow implements IAnimal
 			}
 			else if(familiarity < 30){
 				familiarity -= 2*(TFC_Time.getTotalDays() - lastFamiliarityUpdate);
-				lastFamiliarityUpdate = TFC_Time.getTotalDays();
+				lastFamiliarityUpdate = totalDays;
 			}
 		}
 		if(familiarity > 100)familiarity = 100;
