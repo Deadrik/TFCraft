ac1f497ca301d66d805234ce4ceb0e50e8797a53
Lepeltier kévin <kevin@lepeltier.info>
2014-11-22 07:34:17 +0100

    shape of selection in connection with the chisel mode

--- src/Common/com/bioxx/tfc/Blocks/BlockStair.java
+++ src/Common/com/bioxx/tfc/Blocks/BlockStair.java
@@ -1,5 +1,6 @@
 package com.bioxx.tfc.Blocks;
 
+import java.util.BitSet;
 import java.util.List;
 import java.util.Random;
 
@@ -22,7 +23,6 @@ import com.bioxx.tfc.Items.Tools.ItemHammer;
 
 public class BlockStair extends BlockPartial
 {
-
 	public BlockStair(Material m)
 	{
 		super(m);
@@ -45,71 +45,58 @@ public class BlockStair extends BlockPartial
 	{
 		int meta = world.getBlockMetadata(x, y, z);
 		int rvmeta = meta & 7;
-		float var9 = 0.0F;
-		float var10 = 0.5F;
-		float var11 = 0.5F;
-		float var12 = 1.0F;
+		float var9 = 0;
+		if ((meta & 8) != 0) var9 = .5F;
 
-		if ((meta & 8) != 0)
-		{
-			var9 = 0.5F;
-			var10 = 1.0F;
-			var11 = 0.0F;
-			var12 = 0.5F;
-		}
-
-		this.setBlockBounds(0.0F, var9, 0.0F, 1.0F, var10, 1.0F);
-		super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, 0  ,   0+var9, 0  , 1  , .5F+var9, 1);
 
 		if (rvmeta == 0 || rvmeta == 4)
-		{
-			this.setBlockBounds(0.5F, var11, 0.0F, 1.0F, var12, 1.0F);
-			super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
-		}
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, .5F, .5F-var9, 0  , 1  ,   1-var9, 1);
 		else if (rvmeta == 1 || rvmeta == 5)
-		{
-			this.setBlockBounds(0.0F, var11, 0.0F, 0.5F, var12, 1.0F);
-			super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
-		}
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, 0  , .5F-var9, 0  , .5F,   1-var9, 1);
 		else if (rvmeta == 2 || rvmeta == 6)
-		{
-			this.setBlockBounds(0.0F, var11, 0.5F, 1.0F, var12, 1.0F);
-			super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
-		}
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, 0  , .5F-var9, .5F, 1  ,   1-var9, 1);
 		else if (rvmeta == 3 || rvmeta == 7)
-		{
-			this.setBlockBounds(0.0F, var11, 0.0F, 1.0F, var12, 0.5F);
-			super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
-		}
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, 0  , .5F-var9, 0  , 1  ,   1-var9, .5F);
 		
 		if (rvmeta == 4)
-		{
-			this.setBlockBounds(0.0F, var11, 0.0F, 0.5F, var12, 0.5F);
-			super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
-		}
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, 0  , .5F-var9, 0  , .5F,   1-var9, .5F);
 		else if (rvmeta == 5)
-		{
-			this.setBlockBounds(0.5F, var11, 0.5F, 1.0F, var12, 1.0F);
-			super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
-		}
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, .5F, .5F-var9, .5F, 1  ,   1-var9, 1);
 		else if (rvmeta == 6)
-		{
-			this.setBlockBounds(0.5F, var11, 0.0F, 1.0F, var12, 0.5F);
-			super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
-		}
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, .5F, .5F-var9, 0  , 1  ,   1-var9, .5F);
 		else if (rvmeta == 7)
-		{
-			this.setBlockBounds(0.0F, var11, 0.5F, 0.5F, var12, 1.0F);
-			super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
-		}
+			addCollisionBoxes(world, x, y, z, AABB, list, entity, 0  , .5F-var9, .5F, .5F,   1-var9, 1);
 
-		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+		setBlockBoundsBasedOnSelection(world, x, y, z);
+	}
+	
+	public void addCollisionBoxes(World world, int x, int y, int z, AxisAlignedBB AABB, List list, Entity entity, float bx, float by, float bz, float tx, float ty, float tz)
+	{
+		this.setBlockBounds(bx, by, bz, tx, ty, tz);
+		super.addCollisionBoxesToList(world, x, y, z, AABB, list, entity);
 	}
 
 	@Override
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z)
-	{
-		return super.getCollisionBoundingBoxFromPool(world, x, y, z);
+	public BitSet getData( World world, int x, int y, int z ) {
+		
+		BitSet data = new BitSet(8);
+		int meta = world.getBlockMetadata(x, y, z);
+		
+		if( chiselmode == 1 ) {
+			if( meta !=  8 && meta != 10 && meta != 14 ) data.set((0 * 2 + 0)*2 + 0);
+			if( meta != 10 && meta !=  9 && meta != 13 ) data.set((1 * 2 + 0)*2 + 0);
+			if( meta !=  0 && meta !=  2 && meta !=  6 ) data.set((0 * 2 + 0)*2 + 1);
+			if( meta !=  2 && meta !=  1 && meta !=  5 ) data.set((1 * 2 + 0)*2 + 1);
+			if( meta != 11 && meta !=  8 && meta != 12 ) data.set((0 * 2 + 1)*2 + 0);
+			if( meta !=  9 && meta != 11 && meta != 15 ) data.set((1 * 2 + 1)*2 + 0);
+			if( meta !=  3 && meta !=  0 && meta !=  4 ) data.set((0 * 2 + 1)*2 + 1);
+			if( meta !=  1 && meta !=  3 && meta !=  7 ) data.set((1 * 2 + 1)*2 + 1);
+		} else if ( chiselmode == 3 ) {
+			data = metaToDetailed(meta);
+		}
+		
+		return data;
 	}
 
 	@Override
@@ -125,17 +112,6 @@ public class BlockStair extends BlockPartial
 	}
 
 	@Override
-	public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int i, int j, int k)
-	{
-		return AxisAlignedBB.getBoundingBox(i, j, k, i + 1, j + 1, k + 1);
-	}
-
-	@Override
-	public void setBlockBoundsBasedOnState(IBlockAccess bAccess, int i, int j, int k)
-	{
-		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
-	}
-	@Override
 	public void harvestBlock(World world, EntityPlayer entityplayer, int i, int j, int k, int l)
 	{
 	}
@@ -144,14 +120,22 @@ public class BlockStair extends BlockPartial
 	public boolean isBlockSolid(IBlockAccess world, int x, int y, int z, int side)
 	{
 		boolean solidSides[][] = {
-				{ true, false, false, false, false, true },
-				{ true, false, false, false, true, false },
-				{ true, false, false, true, false, false },
-				{ true, false, true, false, false, false },
-				{ false, true, false, false, false, true },
-				{ false, true, false, false, true, false },
-				{ false, true, false, true, false, false },
-				{ false, true, true, false, false, false }
+				{ true, false, false, false, false, true  },
+				{ true, false, false, false, true , false },
+				{ true, false, false, true , false, false },
+				{ true, false, true , false, false, false },
+				{ true, false, true , false, false, true  },//4
+				{ true, false, false, true , true , false },//5
+				{ true, false, false, true , false, true  },//6
+				{ true, false, true , false, true , false },//7
+				{ false, true, false, false, false, true  },
+				{ false, true, false, false, true , false },
+				{ false, true, false, true , false, false },
+				{ false, true, true , false, false, false },
+				{ false, true, true , false, false, true  },//12
+				{ false, true, false, true , true , false },//13
+				{ false, true, false, true , false, true  },//14
+				{ false, true, true , false, true , false } //15
 		};
 
 		int meta = world.getBlockMetadata(x, y, z);
@@ -188,11 +172,92 @@ public class BlockStair extends BlockPartial
 
 			if(mode == 1)
 			{
-				ItemChisel.ChangeStairs(world, x, y, z, hitX, hitY, hitZ);
+				ChangeStairs(world, x, y, z, hitX, hitY, hitZ);
+				entityplayer.getCurrentEquippedItem().damageItem(1, entityplayer);
+				return true;
+			}
+			else if(mode == 3)
+			{
+				int meta = world.getBlockMetadata(x, y, z);
+				BitSet data = metaToDetailed(meta);
+				data = BlockDetailed.EmptyDetailedInt(xSelected, ySelected, zSelected, data);
+				BlockDetailed.BitSetToDetailled(world, x, y, z, data );
+				data = null;
+				System.gc();
 				entityplayer.getCurrentEquippedItem().damageItem(1, entityplayer);
 				return true;
 			}
 		}
 		return false;
+	}
+
+	public static void ChangeStairs(World world, int x, int y, int z, float hitX, float hitY, float hitZ)
+	{
+		int meta = world.getBlockMetadata(x, y, z);
+		int newmeta = meta;
+		
+		if( hitY > 0.5F ) {
+			if( ( meta == 4 && hitX <= 0.5F && hitZ <= 0.5F )
+			 || ( meta == 6 && hitX <= 0.5F && hitZ >= 0.5F ) ) newmeta = 0;
+			if( ( meta == 7 && hitX >= 0.5F && hitZ <= 0.5F )
+			 || ( meta == 5 && hitX >= 0.5F && hitZ >= 0.5F ) ) newmeta = 1;
+			if( ( meta == 6 && hitX >= 0.5F && hitZ <= 0.5F )
+			 || ( meta == 5 && hitX <= 0.5F && hitZ <= 0.5F ) ) newmeta = 2;
+			if( ( meta == 7 && hitX <= 0.5F && hitZ >= 0.5F )
+			 || ( meta == 4 && hitX >= 0.5F && hitZ >= 0.5F ) ) newmeta = 3;
+		}
+		else
+		{
+			if( ( meta == 12 && hitX <= 0.5F && hitZ <= 0.5F )
+			 || ( meta == 14 && hitX <= 0.5F && hitZ >= 0.5F ) ) newmeta = 8;
+			if( ( meta == 15 && hitX >= 0.5F && hitZ <= 0.5F )
+			 || ( meta == 13 && hitX >= 0.5F && hitZ >= 0.5F ) ) newmeta = 9;
+			if( ( meta == 14 && hitX >= 0.5F && hitZ <= 0.5F )
+			 || ( meta == 13 && hitX <= 0.5F && hitZ <= 0.5F ) ) newmeta = 10;
+			if( ( meta == 15 && hitX <= 0.5F && hitZ >= 0.5F )
+			 || ( meta == 12 && hitX >= 0.5F && hitZ >= 0.5F ) ) newmeta = 11;
+		}
+		
+		if( meta == newmeta ) {
+			BitSet data = metaToDetailed(meta);
+			data.andNot(BlockDetailed.getEmptyQuad(hitX, hitY, hitZ));
+			BitSet quaddata = metaToQuad(meta);
+			BlockDetailed.BitSetToDetailled(world, x, y, z, data, quaddata );
+			data = null;
+			System.gc();
+		} else
+			world.setBlockMetadataWithNotify(x, y, z, newmeta, 0x2);
+	}
+
+	public static BitSet metaToDetailed(int meta)
+	{
+		BitSet data = new BitSet(512);
+		data.set(0, 512);
+		
+		if( meta ==  8 || meta == 10 || meta == 14 ) data.andNot(BlockDetailed.quad_0_0_0);
+		if( meta == 10 || meta ==  9 || meta == 13 ) data.andNot(BlockDetailed.quad_1_0_0);
+		if( meta ==  0 || meta ==  2 || meta ==  6 ) data.andNot(BlockDetailed.quad_0_1_0);
+		if( meta ==  2 || meta ==  1 || meta ==  5 ) data.andNot(BlockDetailed.quad_1_1_0);
+		if( meta == 11 || meta ==  8 || meta == 12 ) data.andNot(BlockDetailed.quad_0_0_1);
+		if( meta ==  9 || meta == 11 || meta == 15 ) data.andNot(BlockDetailed.quad_1_0_1);
+		if( meta ==  3 || meta ==  0 || meta ==  4 ) data.andNot(BlockDetailed.quad_0_1_1);
+		if( meta ==  1 || meta ==  3 || meta ==  7 ) data.andNot(BlockDetailed.quad_1_1_1);
+		
+		return data;
+	}
+
+	private static BitSet metaToQuad(int meta) {
+		BitSet data = new BitSet(8);
+		
+		if( meta ==  8 || meta == 10 || meta == 14 ) data.set((0*2+0)*2+0);
+		if( meta == 10 || meta ==  9 || meta == 13 ) data.set((1*2+0)*2+0);
+		if( meta ==  0 || meta ==  2 || meta ==  6 ) data.set((0*2+0)*2+1);
+		if( meta ==  2 || meta ==  1 || meta ==  5 ) data.set((1*2+0)*2+1);
+		if( meta == 11 || meta ==  8 || meta == 12 ) data.set((0*2+1)*2+0);
+		if( meta ==  9 || meta == 11 || meta == 15 ) data.set((1*2+1)*2+0);
+		if( meta ==  3 || meta ==  0 || meta ==  4 ) data.set((0*2+1)*2+1);
+		if( meta ==  1 || meta ==  3 || meta ==  7 ) data.set((1*2+1)*2+1);
+		
+		return data;
 	}
 }
--- src/Common/com/bioxx/tfc/Core/CollisionRayTraceDetailed.java
+++ src/Common/com/bioxx/tfc/Core/CollisionRayTraceDetailed.java
@@ -6,24 +6,19 @@ import java.util.List;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Vec3;
 
-import com.bioxx.tfc.Blocks.BlockDetailed;
-import com.bioxx.tfc.TileEntities.TEDetailed;
-
 public class CollisionRayTraceDetailed
 {
-	public static List<Object[]> rayTraceSubBlocks(BlockDetailed construct, Vec3 player, Vec3 view, int i, int j, int k, List<Object[]> returns, BitSet data, TEDetailed te)
+	public static List<Object[]> rayTraceSubBlocks(Vec3 player, Vec3 view, int x, int y, int z, List<Object[]> returns, BitSet data, int d)
 	{
-		int d = 8;
-		int dd = (d * d);
 		float div = 1f / d;
 
-		for(int subX = 0; subX < 8; subX++)
+		for(int subX = 0; subX < d; subX++)
 		{
-			for(int subZ = 0; subZ < 8; subZ++)
+			for(int subZ = 0; subZ < d; subZ++)
 			{
-				for(int subY = 0; subY < 8; subY++)
+				for(int subY = 0; subY < d; subY++)
 				{
-					if(te.data.get((subX * 8 + subZ)*8 + subY))
+					if(data.get((subX * d + subZ)*d + subY))
 					{
 						float minX = subX * div;
 						float maxX = minX + div;
@@ -32,7 +27,7 @@ public class CollisionRayTraceDetailed
 						float minZ = subZ * div;
 						float maxZ = minZ + div;
 
-						Object[] ret = construct.rayTraceBound(AxisAlignedBB.getBoundingBox(minX, minY, minZ, maxX, maxY, maxZ), i, j, k, player, view);
+						Object[] ret = rayTraceBound(AxisAlignedBB.getBoundingBox(minX, minY, minZ, maxX, maxY, maxZ), x, y, z, player, view);
 						if (ret != null)
 							returns.add(new Object[] {ret[0],ret[1],ret[2], subX, subY, subZ});
 					}
@@ -40,5 +35,80 @@ public class CollisionRayTraceDetailed
 			}
 		}
 		return returns;
+	}
+	
+	public static Object[] rayTraceBound(AxisAlignedBB bound, int x, int y, int z, Vec3 player, Vec3 view) {
+		Vec3 minX = player.getIntermediateWithXValue(view, bound.minX);
+		Vec3 maxX = player.getIntermediateWithXValue(view, bound.maxX);
+		Vec3 minY = player.getIntermediateWithYValue(view, bound.minY);
+		Vec3 maxY = player.getIntermediateWithYValue(view, bound.maxY);
+		Vec3 minZ = player.getIntermediateWithZValue(view, bound.minZ);
+		Vec3 maxZ = player.getIntermediateWithZValue(view, bound.maxZ);
+		
+		if (!isVecInsideYZBounds(bound, minX))
+			minX = null;
+		if (!isVecInsideYZBounds(bound, maxX))
+			maxX = null;
+		if (!isVecInsideXZBounds(bound, minY))
+			minY = null;
+		if (!isVecInsideXZBounds(bound, maxY))
+			maxY = null;
+		if (!isVecInsideXYBounds(bound, minZ))
+			minZ = null;
+		if (!isVecInsideXYBounds(bound, maxZ))
+			maxZ = null;
+
+		Vec3 tracedBound = null;
+		if (minX != null && (tracedBound == null || player.distanceTo(minX) < player.distanceTo(tracedBound)))
+			tracedBound = minX;
+		if (maxX != null && (tracedBound == null || player.distanceTo(maxX) < player.distanceTo(tracedBound)))
+			tracedBound = maxX;
+		if (minY != null && (tracedBound == null || player.distanceTo(minY) < player.distanceTo(tracedBound)))
+			tracedBound = minY;
+		if (maxY != null && (tracedBound == null || player.distanceTo(maxY) < player.distanceTo(tracedBound)))
+			tracedBound = maxY;
+		if (minZ != null && (tracedBound == null || player.distanceTo(minZ) < player.distanceTo(tracedBound)))
+			tracedBound = minZ;
+		if (maxZ != null && (tracedBound == null || player.distanceTo(maxZ) < player.distanceTo(tracedBound)))
+			tracedBound = maxZ;
+		if (tracedBound == null)
+			return null;
+
+		byte side = -1;
+		if (tracedBound == minX)
+			side = 4;
+		if (tracedBound == maxX)
+			side = 5;
+		if (tracedBound == minY)
+			side = 0;
+		if (tracedBound == maxY)
+			side = 1;
+		if (tracedBound == minZ)
+			side = 2;
+		if (tracedBound == maxZ)
+			side = 3;
+
+		return new Object[] { tracedBound, side, player.distanceTo(tracedBound) };
+	}
+
+	private static boolean isVecInsideYZBounds(AxisAlignedBB bound, Vec3 Vec3) {
+		if (Vec3 == null)
+			return false;
+		else
+			return Vec3.yCoord >= bound.minY && Vec3.yCoord <= bound.maxY && Vec3.zCoord >= bound.minZ && Vec3.zCoord <= bound.maxZ;
+	}
+
+	private static boolean isVecInsideXZBounds(AxisAlignedBB bound, Vec3 Vec3) {
+		if (Vec3 == null)
+			return false;
+		else
+			return Vec3.xCoord >= bound.minX && Vec3.xCoord <= bound.maxX && Vec3.zCoord >= bound.minZ && Vec3.zCoord <= bound.maxZ;
+	}
+
+	private static boolean isVecInsideXYBounds(AxisAlignedBB bound, Vec3 Vec3) {
+		if (Vec3 == null)
+			return false;
+		else
+			return Vec3.xCoord >= bound.minX && Vec3.xCoord <= bound.maxX && Vec3.yCoord >= bound.minY && Vec3.yCoord <= bound.maxY;
 	}
 }
