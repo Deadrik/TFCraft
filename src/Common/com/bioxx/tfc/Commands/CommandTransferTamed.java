f1cbf466b653239d53da4644beb65ad5b7b08005
Bioxx <bioxx2007@gmail.com>
2014-11-24 23:47:06 -0500

    cleanup

--- src/Common/com/bioxx/tfc/Commands/CommandTransferTamed.java
+++ src/Common/com/bioxx/tfc/Commands/CommandTransferTamed.java
@@ -1,6 +1,7 @@
 package com.bioxx.tfc.Commands;
 import java.util.Arrays;
 import java.util.List;
+
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.PlayerNotFoundException;
@@ -11,17 +12,16 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.IChatComponent;
 
-public class CommandTransferTamed extends CommandBase{
-
-	private static final String __OBFID = "CL_00000641";
-
+public class CommandTransferTamed extends CommandBase
+{
+	@Override
 	public List getCommandAliases()
 	{
 		return Arrays.asList(new String[] {"transfer"});
 	}
 
+	@Override
 	public String getCommandName()
 	{
 		return "transferTamed";
@@ -30,74 +30,78 @@ public class CommandTransferTamed extends CommandBase{
 	/**
 	 * Return the required permission level for this command.
 	 */
+	@Override
 	public int getRequiredPermissionLevel()
 	{
 		return 0;
 	}
 
+	@Override
 	public String getCommandUsage(ICommandSender p_71518_1_)
 	{
 		return "commands.transferTamed.usage";
 	}
 
+	@Override
 	public void processCommand(ICommandSender sender, String[] chars)
 	{
-			if(sender instanceof EntityPlayer){
-				EntityPlayerMP entityplayermp = null;
-				if(chars.length > 0){
-					entityplayermp = getPlayer(sender, chars[0]);
-				}
-				
-				EntityTameable theEntity = null;
-				List<EntityTameable> entitiesInRange =	((EntityPlayer)sender).worldObj.getEntitiesWithinAABB(EntityTameable.class, ((EntityPlayer)sender).boundingBox.expand(3, 1, 3));
+		if(sender instanceof EntityPlayer){
+			EntityPlayerMP entityplayermp = null;
+			if(chars.length > 0){
+				entityplayermp = getPlayer(sender, chars[0]);
+			}
 
-				if(entitiesInRange.size() == 0){
-					throw new WrongUsageException("commands.transferTamed.noTamed");
-				}
-				else if(entitiesInRange.size() > 1){
-					throw new WrongUsageException("commands.transferTamed.tooMany");
-				}
-				else if(entitiesInRange.size() == 1){
-					theEntity = entitiesInRange.get(0);
-					if(theEntity.getOwner() == null || !theEntity.getOwner().equals(sender)){
-						throw new WrongUsageException("commands.transferTamed.wrongOwner");
-					}
-				}
+			EntityTameable theEntity = null;
+			List<EntityTameable> entitiesInRange =	((EntityPlayer)sender).worldObj.getEntitiesWithinAABB(EntityTameable.class, ((EntityPlayer)sender).boundingBox.expand(3, 1, 3));
 
-				if (entityplayermp == null)
-				{
-					if(theEntity != null && chars.length == 0){
-						theEntity.setTamed(false);
-						theEntity.func_152115_b("");
-					}
-					else{
-						throw new PlayerNotFoundException();
-					}
-				}
-				else if (entityplayermp == sender)
-				{
-					throw new PlayerNotFoundException("commands.transferTamed.sameTarget", new Object[0]);
-				}
-				else
-				{
-					theEntity.func_152115_b(entityplayermp.getUniqueID().toString());
-					
-					ChatComponentTranslation chatcomponenttranslation = new ChatComponentTranslation("commands.transferTamed.display.incoming", new Object[] {sender.func_145748_c_()});
-					ChatComponentTranslation chatcomponenttranslation1 = new ChatComponentTranslation("commands.transferTamed.display.outgoing", new Object[] {entityplayermp.func_145748_c_()});
-					chatcomponenttranslation.getChatStyle().setColor(EnumChatFormatting.GRAY).setItalic(Boolean.valueOf(true));
-					chatcomponenttranslation1.getChatStyle().setColor(EnumChatFormatting.GRAY).setItalic(Boolean.valueOf(true));
-					entityplayermp.addChatMessage(chatcomponenttranslation);
-					sender.addChatMessage(chatcomponenttranslation1);
+			if(entitiesInRange.size() == 0){
+				throw new WrongUsageException("commands.transferTamed.noTamed");
+			}
+			else if(entitiesInRange.size() > 1){
+				throw new WrongUsageException("commands.transferTamed.tooMany");
+			}
+			else if(entitiesInRange.size() == 1){
+				theEntity = entitiesInRange.get(0);
+				if(theEntity.getOwner() == null || !theEntity.getOwner().equals(sender)){
+					throw new WrongUsageException("commands.transferTamed.wrongOwner");
 				}
 			}
-			else{
-				throw new WrongUsageException("commands.transferTamed.wrongSender");
+
+			if (entityplayermp == null)
+			{
+				if(theEntity != null && chars.length == 0){
+					theEntity.setTamed(false);
+					theEntity.func_152115_b("");
+				}
+				else{
+					throw new PlayerNotFoundException();
+				}
 			}
+			else if (entityplayermp == sender)
+			{
+				throw new PlayerNotFoundException("commands.transferTamed.sameTarget", new Object[0]);
+			}
+			else
+			{
+				theEntity.func_152115_b(entityplayermp.getUniqueID().toString());
+
+				ChatComponentTranslation chatcomponenttranslation = new ChatComponentTranslation("commands.transferTamed.display.incoming", new Object[] {sender.func_145748_c_()});
+				ChatComponentTranslation chatcomponenttranslation1 = new ChatComponentTranslation("commands.transferTamed.display.outgoing", new Object[] {entityplayermp.func_145748_c_()});
+				chatcomponenttranslation.getChatStyle().setColor(EnumChatFormatting.GRAY).setItalic(Boolean.valueOf(true));
+				chatcomponenttranslation1.getChatStyle().setColor(EnumChatFormatting.GRAY).setItalic(Boolean.valueOf(true));
+				entityplayermp.addChatMessage(chatcomponenttranslation);
+				sender.addChatMessage(chatcomponenttranslation1);
+			}
+		}
+		else{
+			throw new WrongUsageException("commands.transferTamed.wrongSender");
+		}
 	}
 
 	/**
 	 * Adds the strings available in this command to the given list of tab completion options.
 	 */
+	@Override
 	public List addTabCompletionOptions(ICommandSender p_71516_1_, String[] p_71516_2_)
 	{
 		/**
@@ -110,6 +114,7 @@ public class CommandTransferTamed extends CommandBase{
 	/**
 	 * Return whether the specified command parameter index is a username parameter.
 	 */
+	@Override
 	public boolean isUsernameIndex(String[] p_82358_1_, int p_82358_2_)
 	{
 		return p_82358_2_ == 0;
