83e5d649a8965e43610e7e9eb9a479c82983e033
Kittychanley <Kittychanley@gmail.com>
2015-01-04 01:02:03 -0600

    Fixed arrows "stuck" in blocks going into the inventory instead of the quiver when picked up.

--- src/Common/com/bioxx/tfc/Entities/EntityProjectileTFC.java
+++ src/Common/com/bioxx/tfc/Entities/EntityProjectileTFC.java
@@ -61,23 +61,26 @@ public class EntityProjectileTFC extends EntityArrow implements ICausesDamage
 			boolean inground = nbt.hasKey("inGround") && nbt.getByte("inGround") == 1;
 			if(inground)
 			{
-				boolean flag = this.canBePickedUp == 1 || this.canBePickedUp == 2 && player.capabilities.isCreativeMode;
+				boolean flag = this.canBePickedUp == 1 || (this.canBePickedUp == 2 && player.capabilities.isCreativeMode);
 
 				EntityItem ei = new EntityItem(this.worldObj, this.posX, this.posY, this.posZ, new ItemStack(this.pickupItem, 1, this.damageTaken));
 
+				if (this.canBePickedUp == 1)
+				{
+					EntityItemPickupEvent event = new EntityItemPickupEvent(player, ei);
+
+					if (MinecraftForge.EVENT_BUS.post(event))
+						return;
+				}
+
 				ItemStack itemstack = ei.getEntityItem();
-				if(itemstack.stackSize <= 0) 
+				if (itemstack.stackSize <= 0)
 					flag = true;
 				else if (this.canBePickedUp == 1 && !player.inventory.addItemStackToInventory(itemstack))
 					flag = false;
 
 				if (flag)
 				{
-					EntityItemPickupEvent event = new EntityItemPickupEvent(player, ei);
-
-					if (MinecraftForge.EVENT_BUS.post(event))
-						return;
-
 					this.playSound("random.pop", 0.2F, ((this.rand.nextFloat() - this.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
 					player.onItemPickup(this, 1);
 					this.setDead();
