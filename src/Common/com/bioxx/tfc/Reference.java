659bc1519172fbf0be735f0fae1f94e793b7fe37
Bioxx <bioxx2007@gmail.com>
2015-06-18 20:57:09 -0400

    Version Bump

--- src/Common/com/bioxx/tfc/Reference.java
+++ src/Common/com/bioxx/tfc/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 
 	public static final int VersionMajor = 0;
 	public static final int VersionMinor = 79;
-	public static final int VersionRevision = 18;
+	public static final int VersionRevision = 19;
 
 	public static final String ModVersion = VersionMajor+"."+VersionMinor+"."+VersionRevision;
 
