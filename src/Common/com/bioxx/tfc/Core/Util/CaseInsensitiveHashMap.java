fed8be28c5151efa6c1bd0c47c441975b7cbe538
Dries007 <admin@dries007.net>
2015-08-17 19:36:48 +0200

    Added case insensitive map to be used in WorldGenOre's OreList. This assimilates much of Deadrik//TFCraft#783 by @CHeuberger

---
+++ src/Common/com/bioxx/tfc/Core/Util/CaseInsensitiveHashMap.java
@@ -0,0 +1,97 @@
+package com.bioxx.tfc.Core.Util;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Made for WorldGenOre.OreList, but usefull for other purposes.
+ * Stores all keys in lowercase
+ *
+ * @author Dries007
+ */
+@SuppressWarnings({"unused", "unchecked"})
+public class CaseInsensitiveHashMap<V> extends HashMap<String, V>
+{
+	public CaseInsensitiveHashMap(int initialCapacity, float loadFactor)
+	{
+		super(initialCapacity, loadFactor);
+	}
+
+	public CaseInsensitiveHashMap(int initialCapacity)
+	{
+		super(initialCapacity);
+	}
+
+	public CaseInsensitiveHashMap()
+	{
+		super();
+	}
+
+	public CaseInsensitiveHashMap(Map<? extends String, ? extends V> m)
+	{
+		super((Map<? extends String, ? extends V>) toLowercase(m));
+	}
+
+	// Helper methods
+	private static Map<String, Object> toLowercase(Map<? extends String, ?> m)
+	{
+		ImmutableMap.Builder<String, Object> temp = ImmutableMap.builder();
+		for (Entry<? extends String, ?> entry : m.entrySet())
+		{
+			temp.put(toLowercase(entry.getKey()), entry.getValue());
+		}
+		return temp.build();
+	}
+
+	private static String toString(Object o)
+	{
+		if (o == null) return null;
+		return o.toString();
+	}
+
+	private static String toLowercase(String key)
+	{
+		if (Strings.isNullOrEmpty(key)) return key;
+		return key.toLowerCase(Locale.ENGLISH);
+	}
+
+	// Override java 1.7 methods involving key
+	@Override
+	public V get(Object key)
+	{
+		return this.get(toString(key));
+	}
+
+	@Override
+	public V put(String key, V value)
+	{
+		return super.put(toLowercase(key), value);
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends V> m)
+	{
+		super.putAll((Map<? extends String, ? extends V>) toLowercase(m));
+	}
+
+	@Override
+	public V remove(Object key)
+	{
+		return this.remove(toString(key));
+	}
+
+	// Properly typed get & remove methods.
+	public V get(String key)
+	{
+		return super.get(toLowercase(key));
+	}
+
+	public V remove(String key)
+	{
+		return super.remove(toLowercase(key));
+	}
+}
