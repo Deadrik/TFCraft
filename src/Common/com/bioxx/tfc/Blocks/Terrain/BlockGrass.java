20e9880c5f1c6d48086451d97bbe766d556f7a4e
AnodeCathode <shotorius_dt@hotmail.com>
2015-11-22 16:47:20 -0700

    Decrease rock/stick regen slightly

--- src/Common/com/bioxx/tfc/Blocks/Terrain/BlockGrass.java
+++ src/Common/com/bioxx/tfc/Blocks/Terrain/BlockGrass.java
@@ -232,9 +232,9 @@ public class BlockGrass extends BlockTerra
 								else if (rand.nextInt(15000) == 0)
 									new WorldGenSaplings().generate(world, rand, i, j, k);	
 							}
-							else if (rand.nextInt(15000) == 0 && !WorldGenLooseRocks.rocksNearby(world, i, j, k) && temp < 15)
+							else if (rand.nextInt(20000) == 0 && !WorldGenLooseRocks.rocksNearby(world, i, j, k) && temp < 15)
 								new WorldGenLooseRocks().generateRocks(world, rand, i, j, k);
-							else if (rand.nextInt(15000) == 0 && temp < 15)
+							else if (rand.nextInt(20000) == 0 && temp < 15)
 								new WorldGenLooseRocks().generateSticks(world, rand, i, j, k);
 
 						}
