e12816a079931af2b08052ec6749761df39972d1
AnodeCathode <shotorius_dt@hotmail.com>
2015-12-17 20:34:51 -0700

    Revert change

--- src/Common/com/bioxx/tfc/TileEntities/TEChest.java
+++ src/Common/com/bioxx/tfc/TileEntities/TEChest.java
@@ -285,10 +285,7 @@ public class TEChest extends TileEntityChest implements IInventory
 	{
 		//super.updateEntity();
 		this.ticksSinceSync++;
-		if ((this.ticksSinceSync + this.xCoord + this.yCoord + this.zCoord) % 200 == 0)
-		{
-			TFC_Core.handleItemTicking(this, this.worldObj, xCoord, yCoord, zCoord);	
-		}
+		TFC_Core.handleItemTicking(this, this.worldObj, xCoord, yCoord, zCoord);	
 		this.checkForAdjacentChests();
 
 		if (!this.worldObj.isRemote && this.numPlayersUsing != 0 && (this.ticksSinceSync + this.xCoord + this.yCoord + this.zCoord) % 200 == 0)
